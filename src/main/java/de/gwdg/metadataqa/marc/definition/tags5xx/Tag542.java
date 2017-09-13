90cfbc0a564691b3c694a8f37bb835a1798d7ba1
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-13 16:11:23 +0200

    Adding tags 54x.

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags5xx/Tag542.java
@@ -0,0 +1,62 @@
+package de.gwdg.metadataqa.marc.definition.tags5xx;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.Indicator;
+
+/**
+ * Information Relating to Copyright Status
+ * http://www.loc.gov/marc/bibliographic/bd542.html
+ */
+public class Tag542 extends DataFieldDefinition {
+
+	private static Tag542 uniqueInstance;
+
+	private Tag542() {
+		initialize();
+	}
+
+	public static Tag542 getInstance() {
+		if (uniqueInstance == null)
+			uniqueInstance = new Tag542();
+		return uniqueInstance;
+	}
+
+	private void initialize() {
+
+		tag = "542";
+		label = "Information Relating to Copyright Status";
+		cardinality = Cardinality.Repeatable;
+		ind1 = new Indicator("Privacy").setCodes(
+				" ", "No information provided",
+				"0", "Private",
+				"1", "Not private"
+		);
+		ind2 = new Indicator("");
+		setSubfieldsWithCardinality(
+				"a", "Personal creator", "NR",
+				"b", "Personal creator death date", "NR",
+				"c", "Corporate creator", "NR",
+				"d", "Copyright holder", "R",
+				"e", "Copyright holder contact information", "R",
+				"f", "Copyright statement", "R",
+				"g", "Copyright date", "NR",
+				"h", "Copyright renewal date", "R",
+				"i", "Publication date", "NR",
+				"j", "Creation date", "NR",
+				"k", "Publisher", "R",
+				"l", "Copyright status", "NR",
+				"m", "Publication status", "NR",
+				"n", "Note", "R",
+				"o", "Research date", "NR",
+				"p", "Country of publication or creation", "R",
+				"q", "Supplying agency", "NR",
+				"r", "Jurisdiction of copyright assessment", "NR",
+				"s", "Source of information", "NR",
+				"u", "Uniform Resource Identifier", "R",
+				"3", "Materials specified", "NR",
+				"6", "Linkage", "NR",
+				"8", "Field link and sequence number", "R"
+		);
+	}
+}
---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags5xx/Tag544.java
@@ -0,0 +1,48 @@
+package de.gwdg.metadataqa.marc.definition.tags5xx;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.Indicator;
+
+/**
+ * Location of Other Archival Materials Note
+ * http://www.loc.gov/marc/bibliographic/bd544.html
+ */
+public class Tag544 extends DataFieldDefinition {
+
+	private static Tag544 uniqueInstance;
+
+	private Tag544() {
+		initialize();
+	}
+
+	public static Tag544 getInstance() {
+		if (uniqueInstance == null)
+			uniqueInstance = new Tag544();
+		return uniqueInstance;
+	}
+
+	private void initialize() {
+
+		tag = "544";
+		label = "Location of Other Archival Materials Note";
+		cardinality = Cardinality.Repeatable;
+		ind1 = new Indicator("Relationship").setCodes(
+				" ", "No information provided",
+				"0", "Associated materials",
+				"1", "Related materials"
+		);
+		ind2 = new Indicator("");
+		setSubfieldsWithCardinality(
+				"a", "Custodian", "R",
+				"b", "Address", "R",
+				"c", "Country", "R",
+				"d", "Title", "R",
+				"e", "Provenance", "R",
+				"n", "Note", "R",
+				"3", "Materials specified", "NR",
+				"6", "Linkage", "NR",
+				"8", "Field link and sequence number", "R"
+		);
+	}
+}

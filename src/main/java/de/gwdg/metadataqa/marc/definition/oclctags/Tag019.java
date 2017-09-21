b60482986f27885d47f7ec53b06bfba2f78d69a0
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-21 15:33:53 +0200

    Adding tag 019.

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/oclctags/Tag019.java
@@ -0,0 +1,37 @@
+package de.gwdg.metadataqa.marc.definition.oclctags;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.Indicator;
+
+/**
+ * OCLC Control Number Cross-Reference
+ * http://www.oclc.org/bibformats/en/0xx/019.html
+ */
+public class Tag019 extends DataFieldDefinition {
+
+	private static Tag019 uniqueInstance;
+
+	private Tag019() {
+		initialize();
+	}
+
+	public static Tag019 getInstance() {
+		if (uniqueInstance == null)
+			uniqueInstance = new Tag019();
+		return uniqueInstance;
+	}
+
+	private void initialize() {
+		tag = "019";
+		label = "OCLC Control Number Cross-Reference";
+		mqTag = "OCLCControlNumber";
+		cardinality = Cardinality.Nonrepeatable;
+		ind1 = new Indicator();
+		ind2 = new Indicator();
+		setSubfieldsWithCardinality(
+			"a", "OCLC control number of merged and deleted record", "NR"
+		);
+		getSubfield("a").setMqTag("rdf:value");
+	}
+}

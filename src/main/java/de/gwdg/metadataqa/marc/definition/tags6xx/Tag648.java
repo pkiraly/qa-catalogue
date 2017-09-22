a1e68b301ed322e9ddbec483f4a9a0dd8ea3c755
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-22 15:48:38 +0200

    Fixing 648.

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags6xx/Tag648.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags6xx/Tag648.java
@@ -3,6 +3,7 @@ package de.gwdg.metadataqa.marc.definition.tags6xx;
 import de.gwdg.metadataqa.marc.definition.Cardinality;
 import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
 import de.gwdg.metadataqa.marc.definition.Indicator;
+import de.gwdg.metadataqa.marc.definition.general.codelist.SubjectHeadingAndTermSourceCodes;
 
 /**
  * Subject Added Entry - Chronological Term
@@ -25,15 +26,15 @@ public class Tag648 extends DataFieldDefinition {
 	private void initialize() {
 		tag = "648";
 		label = "Subject Added Entry - Chronological Term";
+		bibframeTag = "Temporal";
 		mqTag = "ChronologicalSubject";
 		cardinality = Cardinality.Repeatable;
+
 		ind1 = new Indicator();
 		ind2 = new Indicator();
+
 		setSubfieldsWithCardinality(
-			"a", "Named event", "NR",
-			"c", "Location of named event", "R",
-			"d", "Date of named event", "NR",
-			"g", "Miscellaneous information", "R",
+			"a", "Chronological term", "NR",
 			"v", "Form subdivision", "R",
 			"x", "General subdivision", "R",
 			"y", "Chronological subdivision", "R",
@@ -44,9 +45,18 @@ public class Tag648 extends DataFieldDefinition {
 			"6", "Linkage", "NR",
 			"8", "Field link and sequence number", "R"
 		);
+
+		getSubfield("2").setCodeList(SubjectHeadingAndTermSourceCodes.getInstance());
+
+		getSubfield("a").setBibframeTag("Temporal").setMqTag("rdf:value");
 		getSubfield("v").setBibframeTag("formGenre").setMqTag("formSubdivision");
 		getSubfield("x").setBibframeTag("topic").setMqTag("generalSubdivision");
 		getSubfield("y").setBibframeTag("temporal").setMqTag("chronologicalSubdivision");
 		getSubfield("z").setBibframeTag("geographic").setMqTag("geographicSubdivision");
+		getSubfield("0").setMqTag("authorityRecordControlNumber");
+		getSubfield("2").setMqTag("source");
+		getSubfield("3").setMqTag("materialsSpecified");
+		getSubfield("6").setBibframeTag("linkage");
+		getSubfield("8").setMqTag("fieldLink");
 	}
 }

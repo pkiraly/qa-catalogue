5df0ad1317c0b21f4233fe0988d9f50d1865a669
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-23 14:05:33 +0200

    Adjust 013.

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags01x/Tag013.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags01x/Tag013.java
@@ -23,11 +23,15 @@ public class Tag013 extends DataFieldDefinition {
 	}
 
 	private void initialize() {
+
 		tag = "013";
 		label = "Patent Control Information";
+		mqTag = "PatentControl";
 		cardinality = Cardinality.Repeatable;
+
 		ind1 = new Indicator();
 		ind2 = new Indicator();
+
 		setSubfieldsWithCardinality(
 			"a", "Number", "NR",
 			"b", "Country", "NR",
@@ -38,5 +42,17 @@ public class Tag013 extends DataFieldDefinition {
 			"6", "Linkage", "NR",
 			"8", "Field link and sequence number", "R"
 		);
+
+		// TODO
+		// $f - Codes from: MARC Code List for Countries and MARC Code List for Organizations.
+
+		getSubfield("a").setMqTag("number");
+		getSubfield("b").setMqTag("country");
+		getSubfield("c").setMqTag("type");
+		getSubfield("d").setMqTag("date");
+		getSubfield("e").setMqTag("status");
+		getSubfield("f").setMqTag("partyToDocument");
+		getSubfield("6").setBibframeTag("linkage");
+		getSubfield("8").setMqTag("fieldLink");
 	}
 }

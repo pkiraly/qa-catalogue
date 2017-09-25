4c8e666b731d3187aaa00c7ee800eb7a852b7a16
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-25 11:47:47 +0200

    Tagging 086.

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags01x/Tag086.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags01x/Tag086.java
@@ -24,15 +24,20 @@ public class Tag086 extends DataFieldDefinition {
 	}
 
 	private void initialize() {
+
 		tag = "086";
 		label = "Government Document Classification Number";
+		bibframeTag = "Classification";
+		mqTag = "GovernmentDocumentClassification";
 		cardinality = Cardinality.Repeatable;
+
 		ind1 = new Indicator("Number source").setCodes(
 			"#", "Source specified in subfield $2",
 			"0", "Superintendent of Documents Classification System",
 			"1", "Government of Canada Publications: Outline of Classification"
-		);
+		).setMqTag("numberSource");
 		ind2 = new Indicator();
+
 		setSubfieldsWithCardinality(
 			"a", "Classification number", "NR",
 			"z", "Canceled/invalid classification number", "R",
@@ -40,6 +45,13 @@ public class Tag086 extends DataFieldDefinition {
 			"6", "Linkage", "NR",
 			"8", "Field link and sequence number", "R"
 		);
+
 		getSubfield("2").setCodeList(ClassificationSchemeSourceCodes.getInstance());
+
+		getSubfield("a").setBibframeTag("rdfs:label").setMqTag("rdf:value");
+		getSubfield("z").setMqTag("canceled");
+		getSubfield("2").setMqTag("source");
+		getSubfield("6").setBibframeTag("linkage");
+		getSubfield("8").setMqTag("fieldLink");
 	}
 }

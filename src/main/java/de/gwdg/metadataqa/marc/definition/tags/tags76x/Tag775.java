37b69bac61474b608b146be23d893d61bd8c1bb8
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-26 18:21:47 +0200

    Tagging 775.

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags76x/Tag775.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags76x/Tag775.java
@@ -24,10 +24,12 @@ public class Tag775 extends DataFieldDefinition {
 	}
 
 	private void initialize() {
+
 		tag = "775";
 		label = "Other Edition Entry";
 		bibframeTag = "OtherEdition";
 		cardinality = Cardinality.Repeatable;
+
 		ind1 = new Indicator("Note controller").setCodes(
 			"0", "Display note",
 			"1", "Do not display note"
@@ -36,6 +38,7 @@ public class Tag775 extends DataFieldDefinition {
 			" ", "Other edition available",
 			"8", "No display constant generated"
 		).setMqTag("displayConstant");
+
 		setSubfieldsWithCardinality(
 			"a", "Main entry heading", "NR",
 			"b", "Edition", "NR",
@@ -63,6 +66,9 @@ public class Tag775 extends DataFieldDefinition {
 			"7", "Control subfield", "NR",
 			"8", "Field link and sequence number", "R"
 		);
+
+		// TODO: this requires position parser!
+		// see http://www.loc.gov/marc/bibliographic/bd76x78x.html
 		getSubfield("7").setCodes(
 			"0", "Type of main entry heading",
 			"1", "Form of name",
@@ -70,6 +76,7 @@ public class Tag775 extends DataFieldDefinition {
 			"3", "Bibliographic level"
 		);
 		getSubfield("4").setCodeList(RelatorCodes.getInstance());
+
 		getSubfield("a").setBibframeTag("rdfs:label").setMqTag("rdf:value");
 		getSubfield("b").setBibframeTag("editionStatement");
 		getSubfield("c").setBibframeTag("qualifier");

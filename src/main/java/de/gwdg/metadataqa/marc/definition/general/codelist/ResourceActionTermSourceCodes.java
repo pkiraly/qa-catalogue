7d99585b0e6af4f2128648ff6a10cb6692c5ecfb
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-28 18:08:59 +0200

    Adding ResourceActionTermSourceCodes

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/general/codelist/ResourceActionTermSourceCodes.java
@@ -0,0 +1,31 @@
+package de.gwdg.metadataqa.marc.definition.general.codelist;
+
+import de.gwdg.metadataqa.marc.Utils;
+
+/**
+ * Resource Action Term Source Codes
+ * https://www.loc.gov/standards/sourcelist/resource-action.html
+ */
+public class ResourceActionTermSourceCodes extends CodeList {
+
+	private void initialize() {
+		codes = Utils.generateCodes(
+			"pda", "    Preservation & digitization actions: terminology for MARC 21 Field 583 (Mountain View, CA: RLG/ARL/LC Task Force on 583 Field)",
+			"pet", "Preservation event types (Washington, DC: Library of Congress)",
+			"stmanf", "    Standard terminology for the MARC 21 Actions Note Field (Washington, DC: Library of Congress, Network Development and MARC Standards Office)"
+		);
+		indexCodes();
+	}
+
+	private static ResourceActionTermSourceCodes uniqueInstance;
+
+	private ResourceActionTermSourceCodes() {
+		initialize();
+	}
+
+	public static ResourceActionTermSourceCodes getInstance() {
+		if (uniqueInstance == null)
+			uniqueInstance = new ResourceActionTermSourceCodes();
+		return uniqueInstance;
+	}
+}

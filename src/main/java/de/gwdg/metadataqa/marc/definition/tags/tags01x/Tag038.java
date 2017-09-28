0bca913fb92408e55d1e8fc40179dd064b99f46a
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-28 11:40:16 +0200

    Tagging 038.

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags01x/Tag038.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags01x/Tag038.java
@@ -3,6 +3,7 @@ package de.gwdg.metadataqa.marc.definition.tags.tags01x;
 import de.gwdg.metadataqa.marc.definition.Cardinality;
 import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
 import de.gwdg.metadataqa.marc.definition.Indicator;
+import de.gwdg.metadataqa.marc.definition.general.codelist.OrganizationCodes;
 
 /**
  * Record Content Licensor
@@ -23,16 +24,25 @@ public class Tag038 extends DataFieldDefinition {
 	}
 
 	private void initialize() {
+
 		tag = "038";
 		label = "Record Content Licensor";
 		bibframeTag = "MetadataLicensor";
 		cardinality = Cardinality.Nonrepeatable;
+
 		ind1 = new Indicator();
 		ind2 = new Indicator();
+
 		setSubfieldsWithCardinality(
 			"a", "Record content licensor", "NR",
 			"6", "Linkage", "NR",
 			"8", "Field link and sequence number", "R"
 		);
+
+		getSubfield("a").setCodeList(OrganizationCodes.getInstance());
+
+		getSubfield("a").setMqTag("rdf:value");
+		getSubfield("6").setBibframeTag("linkage");
+		getSubfield("8").setMqTag("fieldLink");
 	}
 }

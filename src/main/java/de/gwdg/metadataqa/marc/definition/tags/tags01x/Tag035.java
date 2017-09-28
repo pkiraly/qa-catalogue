a0c525f2f1da3c713c89f5ce570c024075388092
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-28 11:34:19 +0200

    Tagging 035.

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags01x/Tag035.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags01x/Tag035.java
@@ -6,8 +6,8 @@ import de.gwdg.metadataqa.marc.definition.Indicator;
 import de.gwdg.metadataqa.marc.definition.general.parser.RecordControlNumberParser;
 
 /**
- * Musical Incipits Information
- * http://www.loc.gov/marc/bibliographic/bd031.html
+ * System Control Number
+ * http://www.loc.gov/marc/bibliographic/bd035.html
  */
 public class Tag035 extends DataFieldDefinition {
 
@@ -24,19 +24,28 @@ public class Tag035 extends DataFieldDefinition {
 	}
 
 	private void initialize() {
+
 		tag = "035";
 		label = "System Control Number";
-		bibframeTag = "SystemControlNumber";
+		bibframeTag = "IdentifiedBy/Local";
+		mqTag = "SystemControlNumber";
 		cardinality = Cardinality.Repeatable;
+
 		ind1 = new Indicator();
 		ind2 = new Indicator();
+
 		setSubfieldsWithCardinality(
 			"a", "System control number", "NR",
 			"z", "Canceled/invalid control number", "R",
 			"6", "Linkage", "NR",
 			"8", "Field link and sequence number", "R"
 		);
+
 		getSubfield("a").setContentParser(RecordControlNumberParser.getInstance());
+
 		getSubfield("a").setBibframeTag("rdf:value");
+		getSubfield("z").setMqTag("canceled");
+		getSubfield("6").setBibframeTag("linkage");
+		getSubfield("8").setMqTag("fieldLink");
 	}
 }

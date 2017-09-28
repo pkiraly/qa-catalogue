93b91ad29741fccab5fe34b89d7d33356c36ead3
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-28 15:36:08 +0200

    Tagging 516.

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags5xx/Tag516.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags5xx/Tag516.java
@@ -26,16 +26,23 @@ public class Tag516 extends DataFieldDefinition {
 
 		tag = "516";
 		label = "Type of Computer File or Data Note";
+		mqTag = "TypeOfComputerFileOrData";
 		cardinality = Cardinality.Repeatable;
+
 		ind1 = new Indicator("Display constant controller").setCodes(
 			" ", "Type of file",
 			"8", "No display constant generated"
 		).setMqTag("displayConstant");
 		ind2 = new Indicator();
+
 		setSubfieldsWithCardinality(
 			"a", "Type of computer file or data note", "NR",
 			"6", "Linkage", "NR",
 			"8", "Field link and sequence number", "R"
 		);
+
+		getSubfield("a").setBibframeTag("rdfs:label").setMqTag("rdf:value");
+		getSubfield("6").setBibframeTag("linkage");
+		getSubfield("8").setMqTag("fieldLink");
 	}
 }

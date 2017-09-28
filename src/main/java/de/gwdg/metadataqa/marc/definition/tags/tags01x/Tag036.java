982a0c3bfd34d14c0933b0b7f06d465bb29c08cc
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-28 11:36:35 +0200

    Tagging 036.

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags01x/Tag036.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags01x/Tag036.java
@@ -23,18 +23,25 @@ public class Tag036 extends DataFieldDefinition {
 	}
 
 	private void initialize() {
+
 		tag = "036";
 		label = "Original Study Number for Computer Data Files";
 		bibframeTag = "StudyNumber";
 		cardinality = Cardinality.Repeatable;
+
 		ind1 = new Indicator();
 		ind2 = new Indicator();
+
 		setSubfieldsWithCardinality(
 			"a", "Original study number", "NR",
 			"b", "Source agency assigning number", "NR",
 			"6", "Linkage", "NR",
 			"8", "Field link and sequence number", "R"
 		);
+
 		getSubfield("a").setBibframeTag("rdf:value");
+		getSubfield("b").setBibframeTag("source");
+		getSubfield("6").setBibframeTag("linkage");
+		getSubfield("8").setMqTag("fieldLink");
 	}
 }

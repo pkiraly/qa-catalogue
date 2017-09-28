4e0f23fcda72c5a9752671c0949f4a53a1118fbf
Peter Kiraly <peter.kiraly@gwdg.de>
2017-09-28 16:23:06 +0200

    Tagging 581.

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags5xx/Tag581.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/tags5xx/Tag581.java
@@ -26,12 +26,15 @@ public class Tag581 extends DataFieldDefinition {
 
 		tag = "581";
 		label = "Publications About Described Materials Note";
+		mqTag = "PublicationsAboutDescribedMaterials";
 		cardinality = Cardinality.Repeatable;
+
 		ind1 = new Indicator("Display constant controller").setCodes(
 			" ", "Publications",
 			"8", "No display constant generated"
 		).setMqTag("displayConstant");
 		ind2 = new Indicator();
+
 		setSubfieldsWithCardinality(
 			"a", "Publications about described materials note", "NR",
 			"z", "International Standard Book Number", "R",
@@ -39,5 +42,11 @@ public class Tag581 extends DataFieldDefinition {
 			"6", "Linkage", "NR",
 			"8", "Field link and sequence number", "R"
 		);
+
+		getSubfield("a").setBibframeTag("rdfs:label").setMqTag("rdf:value");
+		getSubfield("z").setMqTag("isbn");
+		getSubfield("3").setMqTag("materialsSpecified");
+		getSubfield("6").setBibframeTag("linkage");
+		getSubfield("8").setMqTag("fieldLink");
 	}
 }

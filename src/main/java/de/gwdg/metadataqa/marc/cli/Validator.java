3f5ad13382cac23e5bb149895bb4fa6c39ff2e69
Peter Kiraly <peter.kiraly@gwdg.de>
2017-10-27 23:04:31 +0200

    Issue #7: validating multiple files.

--- src/main/java/de/gwdg/metadataqa/marc/cli/Validator.java
+++ src/main/java/de/gwdg/metadataqa/marc/cli/Validator.java
@@ -15,6 +15,7 @@ import org.marc4j.MarcReader;
 import org.marc4j.marc.Record;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalTime;
@@ -49,88 +50,97 @@ public class Validator {
 		Map<String, Integer> errorCounter = new TreeMap<>();
 
 		MarcVersion marcVersion = parameters.getMarcVersion();
-		System.err.println("marcVersion: " + marcVersion.getCode() + ", " + marcVersion.getLabel());
+		logger.info("marcVersion: " + marcVersion.getCode() + ", " + marcVersion.getLabel());
 
 		int limit = parameters.getLimit();
-		System.err.println("limit: " + limit);
+		logger.info("limit: " + limit);
 
 		int offset = parameters.getOffset();
-		System.err.println("offset: " + offset);
+		logger.info("offset: " + offset);
 
-		String relativeFileName = cmd.getArgs()[0];
-		System.err.println("relativeFileName: " + relativeFileName);
-		Path path = Paths.get(relativeFileName);
-		String fileName = path.getFileName().toString();
+		logger.info("MARC files: " + StringUtils.join(cmd.getArgs(), ", "));
 
-		JsonPathCache<? extends XmlFieldInstance> cache;
-		List<String> records;
-		try {
-			MarcReader reader = ReadMarc.getReader(path.toString());
+		File output = new File(parameters.getFileName());
+		if (output.exists())
+			output.delete();
 
-			File output = new File(parameters.getFileName());
-			if (output.exists())
-				output.delete();
+		String[] inputFileNames = cmd.getArgs();
 
-			int i = 0;
-			while (reader.hasNext()) {
-				i++;
-				if (isUnderOffset(offset, i)) {
-					continue;
-				}
-				if (isOverLimit(limit, i)) {
-					break;
-				}
+		int i = 0;
+		for (String inputFileName : inputFileNames) {
+			Path path = Paths.get(inputFileName);
+			String fileName = path.getFileName().toString();
+			logger.info("processing: " + fileName);
 
-				Record marc4jRecord = reader.next();
-				try {
-					MarcRecord marcRecord = MarcFactory.createFromMarc4j(marc4jRecord);
-					boolean isValid = marcRecord.validate(marcVersion);
-					if (!isValid) {
-						if (cmd.hasOption("summary")) {
-							for (String error : marcRecord.getErrors()) {
-								if (!errorCounter.containsKey(error)) {
-									errorCounter.put(error, 0);
-								}
-								errorCounter.put(error, errorCounter.get(error) + 1);
-							}
-						} else {
-							String message = String.format(
-								"%s in '%s': \n\t%s\n",
-								(marcRecord.getErrors().size() == 1 ? "Error" : "Errors"),
-								marcRecord.getControl001().getContent(),
-								StringUtils.join(marcRecord.getErrors(), "\n\t")
-							);
-							FileUtils.writeStringToFile(output, message, true);
-						}
+			try {
+				MarcReader reader = ReadMarc.getReader(path.toString());
+				while (reader.hasNext()) {
+					i++;
+					if (isUnderOffset(offset, i)) {
+						continue;
+					}
+					if (isOverLimit(limit, i)) {
+						break;
 					}
 
-					if (i % 10000 == 0)
-						logger.info(String.format("%s/%d) %s", fileName, i, marcRecord.getId()));
-				} catch (IllegalArgumentException e) {
-					logger.severe(String.format("Error with record '%s'. %s", marc4jRecord.getControlNumber(), e.getMessage()));
-					continue;
+					Record marc4jRecord = reader.next();
+					try {
+						MarcRecord marcRecord = MarcFactory.createFromMarc4j(marc4jRecord);
+						boolean isValid = marcRecord.validate(marcVersion);
+						if (!isValid) {
+							if (cmd.hasOption("summary")) {
+								for (String error : marcRecord.getErrors()) {
+									if (!errorCounter.containsKey(error)) {
+										errorCounter.put(error, 0);
+									}
+									errorCounter.put(error, errorCounter.get(error) + 1);
+								}
+							} else {
+								String message = String.format(
+									"%s in '%s': \n\t%s\n",
+									(marcRecord.getErrors().size() == 1 ? "Error" : "Errors"),
+									marcRecord.getControl001().getContent(),
+									StringUtils.join(marcRecord.getErrors(), "\n\t")
+								);
+								FileUtils.writeStringToFile(output, message, true);
+							}
+						}
+
+						if (i % 10000 == 0)
+							logger.info(String.format("%s/%d) %s", fileName, i, marcRecord.getId()));
+					} catch (IllegalArgumentException e) {
+						logger.severe(String.format("Error with record '%s'. %s", marc4jRecord.getControlNumber(), e.getMessage()));
+						continue;
+					}
 				}
+				logger.info(String.format("End of cycle. Validated %d records.", i));
+
+			} catch(SolrServerException ex){
+				logger.severe(ex.toString());
+				System.exit(0);
+			} catch(Exception ex){
+				logger.severe(ex.toString());
+				ex.printStackTrace();
+				System.exit(0);
 			}
+		}
 
-			logger.info(String.format("End of cycle. Validated %d records.", i));
-
-			if (cmd.hasOption("summary")) {
+		if (cmd.hasOption("summary")) {
+			try {
 				for (String error : errorCounter.keySet()) {
 					FileUtils.writeStringToFile(output, String.format("%s (%d times)\n", error, errorCounter.get(error)), true);
 				}
+			} catch (IOException ex) {
+				logger.severe(ex.toString());
+				ex.printStackTrace();
+				System.exit(0);
 			}
-		} catch(SolrServerException ex){
-			logger.severe(ex.toString());
-			System.exit(0);
-		} catch(Exception ex){
-			logger.severe(ex.toString());
-			ex.printStackTrace();
-			System.exit(0);
 		}
 
 		long end = System.currentTimeMillis();
 		long duration = (end - start) / 1000;
-		logger.info(String.format("Bye! It took: %s", LocalTime.MIN.plusSeconds(duration).toString()));
+		logger.info(String.format("Bye! It took: %s",
+			LocalTime.MIN.plusSeconds(duration).toString()));
 
 		System.exit(0);
 	}

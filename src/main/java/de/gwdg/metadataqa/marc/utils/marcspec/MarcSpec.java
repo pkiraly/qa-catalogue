96ba0e1e6faf4980f22e6d2954dfc5a63f43eac1
Peter Kiraly <peter.kiraly@gwdg.de>
2017-11-13 15:09:10 +0100

    issue #28: initial implementation of MARCspec.

---
+++ src/main/java/de/gwdg/metadataqa/marc/utils/marcspec/MarcSpec.java
@@ -0,0 +1,360 @@
+package de.gwdg.metadataqa.marc.utils.marcspec;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * Java port of Carsten Klee's PHP MarcSpec class
+ * https://github.com/MARCspec/php-marc-spec/blob/26f33207fbe2745c692a70a2832ca48cfc0d68e8/MarcSpec.php
+ */
+public class MarcSpec {
+
+	private static final Pattern fieldTagPattern = Pattern.compile("[X0-9]{3,3}|LDR");
+	private static final Pattern hasSpacePattern = Pattern.compile("\\s");
+	private static final Pattern indicatorPattern = Pattern.compile("[a-z0-9_]");
+	private static final Pattern subfieldsPattern = Pattern.compile("[a-z0-9!\\\"#$%&'()*+-./:;<=>?]");
+
+	private String fieldTag;
+	private Integer charStart;
+	private Integer charEnd;
+	private Integer charLength;
+	private Map<String, String> subfields = new HashMap<>();
+	private String indicator1;
+	private String indicator2;
+
+	public MarcSpec() {
+	}
+
+	public MarcSpec(String spec) {
+		if (StringUtils.isNotBlank(spec))
+			decode(spec);
+	}
+
+	public void decode(String spec) {
+		checkIfString(spec);
+		validate(spec);
+	}
+
+	public boolean validate(String spec) {
+		checkIfString(spec);
+		clear();
+		spec = StringUtils.trim(spec);
+
+		if (3 > spec.length()) {
+			throw new IllegalArgumentException("Marc spec must be 3 characters at minimum.");
+		}
+		if (hasSpacePattern.matcher(spec).matches()) {
+			throw new IllegalArgumentException(String.format(
+				"For Field Tag of Marc spec no whitespaces are allowed. But \"%s\" given.", spec
+			));
+		}
+		// check and set field tag
+		String fieldTag = spec.substring(0, 3);
+		setFieldTag(fieldTag);
+
+		String dataRef = spec.substring(3);
+		if (!dataRef.equals("")) {
+			if (spec.substring(3, 4).equals("~")) {
+				// check character postion or range
+				String charPos = spec.substring(4);
+				if (!charPos.equals("")) {
+					int[] _charPos = validateCharPos(charPos);
+					if (_charPos.length > 0) {
+						setCharStart(_charPos[0]);
+						if (_charPos.length > 1) {
+							setCharEnd(_charPos[1]);
+						} else {
+							setCharLength(1);
+							setCharEnd(charStart);
+						}
+					}
+				} else {
+					throw new IllegalArgumentException("For character position or range minimum one digit is required. None given.");
+				}
+			} else {
+				String[] _dataRef = validateDataRef(dataRef);
+				if (_dataRef != null && _dataRef.length > 0) {
+					addSubfields(_dataRef[0]);
+					if (_dataRef.length > 1)
+						setIndicators(_dataRef[1]);
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Encode the MarcSpec object as string
+	 * @return
+	 */
+	public String encode() {
+		if (fieldTag == null)
+			throw new RuntimeException("No field tag available. Assuming MarcSpec is not initialized.");
+
+		String marcspec = fieldTag;
+		if (charStart != null) {
+			marcspec += "~" + charStart;
+
+			if (charEnd != null) {
+				marcspec += "-" + charEnd;
+			}
+
+			return marcspec;
+		}
+
+		if (subfields.size() > 0) {
+			marcspec += StringUtils.join(subfields.keySet(), "");
+		}
+
+		if (indicator1 == null && indicator2 == null) {
+			return marcspec;
+		} else {
+			if (indicator2 == null) {
+				return marcspec += "_" + indicator1;
+			} else if (indicator1 == null) {
+				return marcspec += "__" + indicator2;
+			} else {
+				return marcspec += "_" + indicator1 + indicator2;
+			}
+		}
+	}
+
+	public Integer getCharStart() {
+		return charStart;
+	}
+
+	public String getFieldTag() {
+		return fieldTag;
+	}
+
+	/**
+	 * Get the character ending position
+	 * @return
+	 */
+	public Integer getCharEnd() {
+		if (charEnd != null) {
+			return charEnd;
+		} else if (charStart != null && charLength != null) {
+			return charStart + charLength - 1;
+		} else {
+			return null;
+		}
+	}
+
+	public Integer getCharLength() {
+		if (charLength != null) {
+			return charLength;
+		} else if (charStart != null && charEnd != null) {
+			return charEnd - charStart + 1;
+		} else {
+			return null;
+		}
+	}
+
+	public Map<String, String> getSubfields() {
+		return subfields;
+	}
+
+	public String getIndicator1() {
+		return indicator1;
+	}
+
+	public String getIndicator2() {
+		return indicator2;
+	}
+
+	public void setIndicators(String arg) {
+		checkIfString(arg);
+		for (int x = 0; x < arg.length(); x++) {
+			String indicator = arg.substring(x, x+1);
+			if (0 == x) {
+				if(!"_".equals(indicator))
+					setIndicator1(indicator);
+			}
+			if (1 == x) {
+				if(!"_".equals(indicator))
+					setIndicator2(indicator);
+			}
+		}
+	}
+
+	public void setIndicator1(String indicator) {
+		if (validateIndicators(indicator))
+			indicator1 = indicator;
+	}
+
+	public void setIndicator2(String indicator) {
+		if (validateIndicators(indicator))
+			indicator2 = indicator;
+	}
+
+	public boolean addSubfields(String arg) {
+		if (charStart != null || "LDR".equals(fieldTag))
+			return false;
+		checkIfString(arg);
+		for (int x = 0; x < arg.length(); x++) {
+			String chr = arg.substring(x, x+1);
+			subfields.put(chr, chr);
+		}
+		return true;
+
+	}
+
+	private String[] validateDataRef(String dataFieldRef) {
+		checkIfString(dataFieldRef);
+
+		String[] _ref = dataFieldRef.split("_", 2);
+
+		if (validateSubfields(_ref[0])) {
+			if (_ref.length > 1) {
+				validateIndicators(_ref[1]);
+			}
+		}
+		return _ref;
+	}
+
+	private boolean validateIndicators(String indicators) {
+		checkIfString(indicators);
+
+		if(2 < indicators.length()) {
+			throw new IllegalArgumentException(String.format(
+				"For indicators only two characters are allowed. \"%d\" characters given.", indicators.length()));
+		}
+		for (int x = 0; x < indicators.length(); x++) {
+			String chr = indicators.substring(x, x+1);
+			if (!indicatorPattern.matcher(indicators.substring(x, x+1)).matches()) {
+				throw new IllegalArgumentException(String.format(
+					"For indicators only digits, lowercase alphabetic characters and \"_\" are allowed. But \"%s\" given. Problematic part: '%s'.",
+					indicators, chr
+				));
+			}
+		}
+		return true;
+	}
+
+	private boolean validateSubfields(String subfields) {
+		for (int x = 0; x < subfields.length(); x++) {
+			String chr = subfields.substring(x, x+1);
+			if (!subfieldsPattern.matcher(chr).matches()) {
+				throw new IllegalArgumentException(String.format(
+					"For subfields only digits, lowercase alphabetic characters or one of ... are allowed."
+					+ " But '%s' given. Problem: '%s'.", subfields, chr));
+			}
+		}
+		return true;
+	}
+
+	public void setCharLength(int arg) {
+		if (0 < arg) {
+			if (charStart == null) {
+				throw new RuntimeException(
+					"Character start position must be defined first. Use MarcSpec::setCharStart() first to set the character start position.");
+			}
+			charLength = arg;
+			charEnd = charStart + charLength - 1;
+			setCharEnd(charEnd);
+		} else {
+			throw new IllegalArgumentException("Argument must be of type positive int without 0.");
+		}
+	}
+
+	/**
+	 * Set the character starting position
+	 *
+	 * Length of character range automatically is set if character ending position is set.
+	 *
+	 * @param arg
+	 */
+	public void setCharStart(int arg) {
+		if (0 <= arg) {
+			charStart = arg;
+			if (charEnd != null) {
+				charLength = charEnd - charStart + 1;
+			}
+		} else {
+			throw new IllegalArgumentException("Argument must be of type int.");
+		}
+	}
+
+	/**
+	 * Validate a character position or range
+	 *
+	 * @param charPos	The character position or range
+	 * @return An array of character positions
+	 */
+	private int[] validateCharPos(String charPos) {
+		checkIfString(charPos);
+
+		if (charPos.length() < 1) {
+			throw new IllegalArgumentException("For character position or range minimum one digit is required. None given.");
+		}
+		String[] _charPos = charPos.split("-", 3);
+		if (2 < _charPos.length) {
+			throw new IllegalArgumentException(String.format(
+				"For character position or range only digits and one \"-\" is allowed. But \"%s\" given.",
+				charPos
+			));
+		}
+		int[] positions = new int[_charPos.length];
+		for (int i = 0; i < _charPos.length; i++) {
+			positions[i] = Integer.parseInt(_charPos[i]);
+		}
+
+		return positions;
+	}
+
+	/**
+	 * Set the field tag
+	 *
+	 * Provided param gets validated
+	 * @param arg The field tag
+	 */
+	public void setFieldTag(String arg) {
+		if (validateFieldTag(arg))
+			fieldTag = arg;
+	}
+
+	private boolean validateFieldTag(String fieldTag) {
+		checkIfString(fieldTag);
+		if (!fieldTagPattern.matcher(fieldTag).matches()) {
+			throw new IllegalArgumentException(String.format(
+				"For Field Tag of Marc spec only digits, \"X\" or \"LDR\" is allowed. But \"%s\"  given.", fieldTag
+			));
+		}
+		return true;
+	}
+
+	private void clear() {
+		fieldTag = null;
+		charStart = null;
+		charEnd = null;
+		charLength = null;
+		indicator1 = null;
+		indicator2 = null;
+		subfields = new HashMap<>();
+	}
+
+	private void checkIfString(String arg) {
+		if (!(arg instanceof String))
+			throw new IllegalArgumentException(String.format(
+				"Method decode only accepts string as argument. Given %s.", arg.getClass().getSimpleName()
+			));
+	}
+
+	public void setCharEnd(int arg) {
+		if(0 <= arg) {
+			if (charStart == null) {
+				throw new RuntimeException(
+					"Character start position must be defined first. Use MarcSpec::setCharStart() first to set the character start position.");
+			} else {
+				charEnd = arg;
+				charLength = charEnd - charStart + 1;
+			}
+		} else {
+			throw new IllegalArgumentException("Argument must be of type positive int or 0.");
+		}
+	}
+}
---
+++ src/test/java/de/gwdg/metadataqa/marc/utils/marcspec/MarcSpecTest.java
@@ -0,0 +1,173 @@
+package de.gwdg.metadataqa.marc.utils.marcspec;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MarcSpecTest {
+
+	public void decoder(String arg) {
+		MarcSpec ms = new MarcSpec();
+		ms.decode(arg);
+	}
+
+	public MarcSpec marcspec() {
+		return new MarcSpec();
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testInvalidMarcSpec1() {
+		decoder(" 24 ");
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testInvalidMarcSpec2() {
+		decoder("24/");
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testInvalidMarcSpec3() {
+		decoder("24x");
+	}
+
+	@Test
+	public void testValidMarcSpec1() {
+		MarcSpec marcSpec = new MarcSpec();
+		marcSpec.decode("LDR");
+		assertEquals("LDR", marcSpec.getFieldTag());
+
+		marcSpec.decode("245");
+		assertEquals("245", marcSpec.getFieldTag());
+
+		marcSpec.decode("XXX");
+		assertEquals("XXX", marcSpec.getFieldTag());
+
+		marcSpec.decode("245abc");
+		assertEquals("245", marcSpec.getFieldTag());
+		assertEquals("a,b,c", StringUtils.join(marcSpec.getSubfields().keySet(), ","));
+
+		// marcSpec.decode("245!\"#$%&\\()*+-./:;<=>?");
+		// assertEquals("a,b,c", StringUtils.join(marcSpec.getSubfields().keySet(), ","));
+
+		marcSpec.decode("245ab_1a");
+		assertEquals("1", marcSpec.getIndicator1());
+		assertEquals("a", marcSpec.getIndicator2());
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testInvalidMarcSpec4() {
+		decoder("007~");
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testInvalidMarcSpec5() {
+		decoder("007~1-2-");
+	}
+
+	@Test
+	public void testValidMarcSpec2() {
+		MarcSpec marcSpec = new MarcSpec();
+		marcSpec.decode("LDR~0-3");
+		assertEquals("LDR", marcSpec.getFieldTag());
+		assertEquals(0, (int)marcSpec.getCharStart());
+		assertEquals(3, (int)marcSpec.getCharEnd());
+		assertEquals(4, (int)marcSpec.getCharLength());
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testInvalidMarcSpec6() {
+		decoder("245aX");
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testInvalidMarcSpec7() {
+		decoder("245ab_1+");
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testInvalidMarcSpec8() {
+		decoder("245ab_123");
+	}
+
+	@Test
+	public void testSetAndGet() {
+		MarcSpec marcSpec = new MarcSpec();
+		marcSpec.setFieldTag("LDR");
+		marcSpec.setCharStart(0);
+		marcSpec.setCharEnd(3);
+		assertEquals("LDR", marcSpec.getFieldTag());
+		assertEquals(0, (int)marcSpec.getCharStart());
+		assertEquals(3, (int)marcSpec.getCharEnd());
+		assertEquals(4, (int)marcSpec.getCharLength());
+
+		marcSpec = new MarcSpec();
+		marcSpec.setFieldTag("245");
+		marcSpec.addSubfields("abc");
+		marcSpec.setIndicator1("x");
+		marcSpec.setIndicator2("0");
+		assertEquals("245", marcSpec.getFieldTag());
+		assertEquals("x", marcSpec.getIndicator1());
+		assertEquals("0", marcSpec.getIndicator2());
+		// assertEquals(array("a"=>"a","b"=>"b","c"=>"c"), marcSpec.getSubfields());
+	}
+
+	@Test
+	public void testEncode() {
+		MarcSpec marcSpec;
+		marcSpec = new MarcSpec("245");
+		assertEquals("245", marcSpec.encode());
+
+		marcSpec = new MarcSpec("245a");
+		assertEquals("245a", marcSpec.encode());
+
+		marcSpec = new MarcSpec("245_1");
+		assertEquals("245_1", marcSpec.encode());
+
+		marcSpec = new MarcSpec("245__0");
+		assertEquals("245__0", marcSpec.encode());
+
+		marcSpec = new MarcSpec("245_1_");
+		assertEquals("245_1", marcSpec.encode());
+
+		marcSpec = new MarcSpec("007~1");
+		assertEquals("007~1-1", marcSpec.encode());
+
+		marcSpec = new MarcSpec("007~1-3");
+		assertEquals("007~1-3", marcSpec.encode());
+	}
+
+	@Test
+	public void testValidity() {
+		MarcSpec marcSpec = new MarcSpec();
+		assertTrue(marcSpec.validate("245"));
+		assertTrue(marcSpec.validate("245ab"));
+		assertTrue(marcSpec.validate("XXXab"));
+		assertTrue(marcSpec.validate("245ab_1"));
+		assertTrue(marcSpec.validate("245ab__0"));
+		assertTrue(marcSpec.validate("245ab_10"));
+		assertTrue(marcSpec.validate("245ab_1_"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInValidity() {
+		MarcSpec marcSpec = new MarcSpec();
+		assertTrue(marcSpec.validate("24"));
+		assertTrue(marcSpec.validate("LXR"));
+		assertTrue(marcSpec.validate("245ab_123"));
+		assertTrue(marcSpec.validate("245ab__."));
+		assertTrue(marcSpec.validate("004ab~1"));
+		assertTrue(marcSpec.validate("004~-1"));
+	}
+
+
+	@Test
+	public void testCharConsistency1() {
+		MarcSpec marcSpec = new MarcSpec("650");
+		assertEquals(null, marcSpec.getCharStart());
+		assertEquals(null, marcSpec.getCharEnd());
+		assertEquals(null, marcSpec.getCharLength());
+		assertEquals("650", marcSpec.encode());
+	}
+}

4a137a3f10ef2086984044509019f35ad7abdaf6
Peter Kiraly <peter.kiraly@gwdg.de>
2017-12-07 18:25:09 +0100

    Issue #40: defining Finnish marc tags/indicators/subfields.

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/fennicatags/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains the tags specifics at FENNICA
+ *
+ * The source of the classes is this document:
+ *
+ * MARC 21 -formaatti:  BIBLIOGRAFISET TIEDOT
+ * https://www.kansalliskirjasto.fi/extra/marc21/bib/omat.htm#035 %E2%80%A19
+ */
+package de.gwdg.metadataqa.marc.definition.tags.fennicatags;
+
+//rest of the file is empty

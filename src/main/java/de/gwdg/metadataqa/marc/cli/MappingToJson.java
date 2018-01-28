d11077c2a71903112b0c24225bd0f42bc1c80f1f
Peter Kiraly <peter.kiraly@gwdg.de>
2018-01-28 12:58:49 +0100

    Issue #45: first version of MARC21 schema export in JSON.

---
+++ src/main/java/de/gwdg/metadataqa/marc/cli/MappingToJson.java
@@ -0,0 +1,203 @@
+package de.gwdg.metadataqa.marc.cli;
+
+import de.gwdg.metadataqa.marc.*;
+import de.gwdg.metadataqa.marc.definition.*;
+import de.gwdg.metadataqa.marc.utils.MarcTagLister;
+import org.apache.commons.lang.StringUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class MappingToJson {
+
+	private static boolean exportSubfieldCodes = false;
+	private static List<String> nonMarc21TagLibraries = Arrays.asList(
+		"oclctags", "fennicatags", "dnbtags", "sztetags", "genttags", "holdings"
+	);
+
+	public static void main(String[] args) {
+		List<Class<? extends DataFieldDefinition>> tags = MarcTagLister.listTags();
+
+		System.out.println("[");
+
+		System.out.println("{\"tag\":\"Leader\",\"positions\":[");
+		List<String> positions = new ArrayList<>();
+		for (ControlSubfield subfield : LeaderSubfields.getSubfields()) {
+			positions.add(controlSubfieldToJson(subfield));
+		}
+		System.out.println(StringUtils.join(positions, ",\n"));
+		System.out.println("]},");
+
+		System.out.println(toJson(true, "tag", "001", "label", Control001.getLabel()) + ",");
+		System.out.println(toJson(true, "tag", "003", "label", Control003.getLabel()) + ",");
+		System.out.println(toJson(true, "tag", "005", "label", Control005.getLabel()) + ",");
+
+		System.out.printf("{\"tag\":\"006\",\"label\":\"%s\",\"types\":[\n", Control006.getLabel());
+		int i = Control006Subfields.getSubfields().keySet().size();
+		for (Control008Type type : Control006Subfields.getSubfields().keySet()) {
+			System.out.printf("{\"type\":\"%s\",\"positions\":[\n", type.getValue());
+			positions = new ArrayList<>();
+			for (ControlSubfield subfield : Control006Subfields.getSubfields().get(type))
+				positions.add(controlSubfieldToJson(subfield));
+			System.out.println(StringUtils.join(positions, ",\n"));
+			System.out.println("]}" + (--i > 0 ? "," : ""));
+		}
+		System.out.println("]},");
+
+		System.out.printf("{\"tag\":\"007\",\"label\":\"%s\",\"categories\":[\n", Control007.getLabel());
+		i = Control007Subfields.getSubfields().keySet().size();
+		for (Control007Category category : Control007Subfields.getSubfields().keySet()) {
+			System.out.printf("{\"type\":\"%s\",\"positions\":[\n", category.getLabel());
+			positions = new ArrayList<>();
+			for (ControlSubfield subfield : Control007Subfields.getSubfields().get(category))
+				positions.add(controlSubfieldToJson(subfield));
+			System.out.println(StringUtils.join(positions, ",\n"));
+			System.out.println("]}" + (--i > 0 ? "," : ""));
+		}
+		System.out.println("]},");
+
+		System.out.printf("{\"tag\":\"008\",\"label\":\"%s\",\"types\":[", Control008.getLabel());
+		i = Control008Subfields.getSubfields().keySet().size();
+		for (Control008Type type : Control008Subfields.getSubfields().keySet()) {
+			System.out.printf("{\"type\":\"%s\",\"positions\":[\n", type.getValue());
+			positions = new ArrayList<>();
+			for (ControlSubfield subfield : Control008Subfields.getSubfields().get(type))
+				positions.add(controlSubfieldToJson(subfield));
+			System.out.println(StringUtils.join(positions, ",\n"));
+			System.out.println("]}" + (--i > 0 ? "," : ""));
+		}
+		System.out.println("]},");
+
+		List<String> items = new ArrayList<>();
+		for (Class<? extends DataFieldDefinition> tagClass : tags) {
+			if (isNonMarc21Tag(tagClass))
+				continue;
+
+			Method getInstance;
+			DataFieldDefinition tag;
+			try {
+				getInstance = tagClass.getMethod("getInstance");
+				tag = (DataFieldDefinition) getInstance.invoke(tagClass);
+				items.add(tagToJson(tag));
+			} catch (NoSuchMethodException
+			       | IllegalAccessException
+			       | InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(StringUtils.join(items, ",\n"));
+		System.out.println("]");
+	}
+
+	private static boolean isNonMarc21Tag(Class<? extends DataFieldDefinition> tagClass) {
+		boolean isNonMarc21Tag = false;
+		for (String nonCore : nonMarc21TagLibraries) {
+			if (tagClass.getCanonicalName().contains(nonCore)) {
+				isNonMarc21Tag = true;
+				break;
+			}
+		}
+		return isNonMarc21Tag;
+	}
+
+	private static String controlSubfieldToJson(ControlSubfield subfield) {
+		Map<String, String> values = new LinkedHashMap<>();
+		values.put("position", subfield.formatPositon());
+		values.put("label", subfield.getLabel());
+		values.put("URL", subfield.getDescriptionUrl());
+		String text = mapToJson(values, false);
+
+		if (subfield.getCodes() != null) {
+			List<String> codes = new ArrayList<>();
+			for (Code code : subfield.getCodes()) {
+				codes.add(toJson(true, "code", code.getCode(), "label", code.getLabel()));
+			}
+			text += ",\"codes\":[\n" + StringUtils.join(codes, ",\n") + "\n]";
+		}
+		return "{" + text + "}";
+	}
+
+	private static String mapToJson(Map<String, String> values, boolean isAUnit) {
+		List<String> entries = new ArrayList<>();
+		for (Map.Entry value : values.entrySet()) {
+			entries.add(
+				String.format("\"%s\":\"%s\"", value.getKey(), value.getValue())
+			);
+		}
+		if (isAUnit)
+			return '{' + StringUtils.join(entries, ",") + '}';
+		else
+			return StringUtils.join(entries, ",");
+	}
+
+	private static String tagToJson(DataFieldDefinition tag) {
+		String text = "{" + toJson(false,
+			"tag", tag.getTag(),
+			"label", tag.getLabel(),
+			"URL", tag.getDescriptionUrl(),
+			"cardinality", (tag.getCardinality().getCode().equals("R")
+				? "repeatable" : "non-repeatable")
+		);
+		if (tag.getInd1().exists() || tag.getInd2().exists()) {
+			text += ",\"indicators\":{";
+
+			List<String> indicators = new ArrayList<>();
+			if (tag.getInd1().exists())
+				indicators.add(indicatorToJson("ind1", tag.getInd1()));
+
+			if (tag.getInd2().exists())
+				indicators.add(indicatorToJson("ind2", tag.getInd2()));
+			text += StringUtils.join(indicators, ",\n");
+			text += "}\n";
+		}
+
+		text += ",\"subfields\":[\n";
+		List<String> subfields = new ArrayList<>();
+		for (SubfieldDefinition subfield : tag.getSubfields()) {
+			subfields.add(subfieldToJson(subfield));
+		}
+		text += StringUtils.join(subfields, ",\n");
+		text += "]";
+		text += "}";
+
+		return text;
+	}
+
+	private static String subfieldToJson(SubfieldDefinition subfield) {
+		String text = '{' + toJson(false,
+			"code", subfield.getCode(),
+			"label", subfield.getLabel(),
+			"cardinality", (subfield.getCardinalityCode().equals("R")
+				? "repeatable" : "non-repeatable")
+		);
+
+		if (MappingToJson.exportSubfieldCodes) {
+			if (subfield.getCodeList() != null
+				&& !subfield.getCodeList().getCodes().isEmpty()) {
+				List<String> codes = new ArrayList<>();
+				for (Code code : subfield.getCodeList().getCodes()) {
+					codes.add(code.getCode());
+				}
+				text += ",\"codes\":\"" + StringUtils.join(codes, "\",\"") + "\"";
+			}
+		}
+		text += "}";
+		return text;
+	}
+
+	private static String indicatorToJson(String label, Indicator indicator) {
+		String text = String.format("\"%s\":{", label);
+		text += String.format("\"label\": \"%s\"", indicator.getLabel());
+		text += "}";
+		return text;
+	}
+
+	private static String toJson(boolean isAUnit, String... args) {
+		Map<String, String> values = new LinkedHashMap<>();
+		for (int i = 0; i < args.length; i += 2) {
+			values.put(args[i], args[i+1]);
+		}
+		return mapToJson(values, isAUnit);
+	}
+}

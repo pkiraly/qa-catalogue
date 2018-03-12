6a1abdb6c78cd9338e8bea554a3552d10c6a757b
Peter Kiraly <peter.kiraly@gwdg.de>
2018-03-12 10:58:41 +0100

    Issue #48: testing control 006 field

--- src/test/java/de/gwdg/metadataqa/marc/Control006Test.java
+++ src/test/java/de/gwdg/metadataqa/marc/Control006Test.java
@@ -7,6 +7,7 @@ import de.gwdg.metadataqa.marc.definition.controlsubfields.Control006Subfields;
 import de.gwdg.metadataqa.marc.definition.controlsubfields.ControlSubfieldList;
 import de.gwdg.metadataqa.marc.definition.controlsubfields.tag006.Tag006all00;
 import de.gwdg.metadataqa.marc.definition.controlsubfields.tag006.Tag006continuing01;
+import de.gwdg.metadataqa.marc.definition.controlsubfields.tag006.Tag006visual16;
 import de.gwdg.metadataqa.marc.definition.controltype.Control008Type;
 import de.gwdg.metadataqa.marc.definition.controltype.ControlType;
 import de.gwdg.metadataqa.marc.definition.tags.control.Control006Definition;
@@ -305,4 +306,320 @@ public class Control006Test {
 		assertEquals("Technique", field.getValuesList().get(6).getDefinition().getLabel());
 		assertEquals("Unknown", field.getValuesList().get(6).resolve());
 	}
+
+	@Test
+	public void testResolveWithDefinition() {
+		Control006 field = new Control006("r|||            ou", Leader.Type.VISUAL_MATERIALS);
+		assertEquals("Flash card", field.resolve(Tag006visual16.getInstance()));
+	}
+
+	@Test
+	public void testMap() {
+		Control006 field = new Control006("r|||            ou", Leader.Type.VISUAL_MATERIALS);
+		assertEquals(field.getValueMap(), field.getMap());
+	}
+
+	@Test
+	public void testGetValueByPosition() {
+		Control006 field = new Control006("r|||            ou", Leader.Type.VISUAL_MATERIALS);
+		assertEquals("o", field.getValueByPosition(16));
+	}
+
+	@Test
+	public void testGetSubfieldByPosition() {
+		Control006 field = new Control006("r|||            ou", Leader.Type.VISUAL_MATERIALS);
+		assertEquals("tag006visual16", field.getSubfieldByPosition(16).getId());
+		assertEquals("Type of visual material", field.getSubfieldByPosition(16).getLabel());
+	}
+
+	@Test
+	public void testBookGetters() {
+		Control006 field = new Control006("a|||||||||||||||p|", Leader.Type.BOOKS);
+		assertEquals("ControlValue", field.getTag006book01().getClass().getSimpleName());
+
+		// assertEquals("Form of material", field.getValuesList().get(0).getDefinition().getLabel());
+		// assertEquals("Language material", field.getValuesList().get(0).resolve());
+
+		ControlValue value;
+
+		value = field.getTag006book01();
+		assertEquals("Illustrations", value.getDefinition().getLabel());
+		assertEquals("||||", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006book05();
+		assertEquals("Target audience", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006book06();
+		assertEquals("Form of item", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006book07();
+		assertEquals("Nature of contents", value.getDefinition().getLabel());
+		assertEquals("||||", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006book11();
+		assertEquals("Government publication", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006book12();
+		assertEquals("Conference publication", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006book13();
+		assertEquals("Festschrift", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006book14();
+		assertEquals("Index", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006book16();
+		assertEquals("Literary form", value.getDefinition().getLabel());
+		assertEquals("p", value.getValue());
+		assertEquals("Poetry", value.resolve());
+
+		value = field.getTag006book17();
+		assertEquals("Biography", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+	}
+
+	@Test
+	public void testComputerFilesGetters() {
+		Control006 field = new Control006("m    bq  i |      ", Leader.Type.COMPUTER_FILES);
+
+		ControlValue value = field.getTag006all00();
+		assertEquals("Form of material", value.getDefinition().getLabel());
+		assertEquals("m", value.getValue());
+		assertEquals("Computer file", value.resolve());
+
+		value = field.getTag006computer05();
+		assertEquals("Target audience", value.getDefinition().getLabel());
+		assertEquals("b", value.getValue());
+		assertEquals("Primary", value.resolve());
+
+		value = field.getTag006computer06();
+		assertEquals("Form of item", value.getDefinition().getLabel());
+		assertEquals("q", value.getValue());
+		assertEquals("Direct electronic", value.resolve());
+
+		value = field.getTag006computer09();
+		assertEquals("Type of computer file", value.getDefinition().getLabel());
+		assertEquals("i", value.getValue());
+		assertEquals("Interactive multimedia", value.resolve());
+
+		value = field.getTag006computer11();
+		assertEquals("Government publication", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+	}
+
+	@Test
+	public void testMapGetters() {
+		Control006 field = new Control006("e|||||||a|||||||||", Leader.Type.MAPS);
+
+		ControlValue value = field.getTag006all00();
+		assertEquals("Form of material", value.getDefinition().getLabel());
+		assertEquals("e", value.getValue());
+		assertEquals("Cartographic material", value.resolve());
+
+		value = field.getTag006map01();
+		assertEquals("Relief", value.getDefinition().getLabel());
+		assertEquals("||||", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006map05();
+		assertEquals("Projection", value.getDefinition().getLabel());
+		assertEquals("||", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006map08();
+		assertEquals("Type of cartographic material", value.getDefinition().getLabel());
+		assertEquals("a", value.getValue());
+		assertEquals("Single map", value.resolve());
+
+		value = field.getTag006map11();
+		assertEquals("Government publication", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006map12();
+		assertEquals("Form of item", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006map14();
+		assertEquals("Index", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006map16();
+		assertEquals("Special format characteristics", value.getDefinition().getLabel());
+		assertEquals("||", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+	}
+
+	@Test
+	public void testMusicGetters() {
+		Control006 field = new Control006("jfmnn        nn | ", Leader.Type.MUSIC);
+
+		ControlValue value = field.getTag006all00();
+		assertEquals("Form of material", value.getDefinition().getLabel());
+		assertEquals("j", value.getValue());
+		assertEquals("Musical sound recording", value.resolve());
+
+		value = field.getTag006music01();
+		assertEquals("Form of composition", value.getDefinition().getLabel());
+		assertEquals("fm", value.getValue());
+		assertEquals("Folk music", value.resolve());
+
+		value = field.getTag006music03();
+		assertEquals("Format of music", value.getDefinition().getLabel());
+		assertEquals("n", value.getValue());
+		assertEquals("Not applicable", value.resolve());
+
+		value = field.getTag006music04();
+		assertEquals("Music parts", value.getDefinition().getLabel());
+		assertEquals("n", value.getValue());
+		assertEquals("Not applicable", value.resolve());
+
+		value = field.getTag006music05();
+		assertEquals("Target audience", value.getDefinition().getLabel());
+		assertEquals(" ", value.getValue());
+		assertEquals("Unknown or unspecified", value.resolve());
+
+		value = field.getTag006music06();
+		assertEquals("Form of item", value.getDefinition().getLabel());
+		assertEquals(" ", value.getValue());
+		assertEquals("None of the following", value.resolve());
+
+		value = field.getTag006music07();
+		assertEquals("Accompanying matter", value.getDefinition().getLabel());
+		assertEquals("      ", value.getValue());
+		assertEquals("No accompanying matter", value.resolve());
+
+		value = field.getTag006music13();
+		assertEquals("Literary text for sound recordings", value.getDefinition().getLabel());
+		assertEquals("nn", value.getValue());
+		assertEquals("Not applicable", value.resolve());
+
+		value = field.getTag006music16();
+		assertEquals("Transposition and arrangement", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+	}
+
+	@Test
+	public void testContinuingResourcesGetters() {
+		Control006 field = new Control006("swr n|      0   b0", Leader.Type.CONTINUING_RESOURCES);
+		ControlValue value = field.getTag006all00();
+		assertEquals("Form of material", value.getDefinition().getLabel());
+		assertEquals("s", value.getValue());
+		assertEquals("Serial/Integrating resource", value.resolve());
+
+		value = field.getTag006continuing01();
+		assertEquals("Frequency", value.getDefinition().getLabel());
+		assertEquals("w", value.getValue());
+		assertEquals("Weekly", value.resolve());
+
+		value = field.getTag006continuing02();
+		assertEquals("Regularity", value.getDefinition().getLabel());
+		assertEquals("r", value.getValue());
+		assertEquals("Regular", value.resolve());
+
+		value = field.getTag006continuing04();
+		assertEquals("Type of continuing resource", value.getDefinition().getLabel());
+		assertEquals("n", value.getValue());
+		assertEquals("Newspaper", value.resolve());
+
+		value = field.getTag006continuing05();
+		assertEquals("Form of original item", value.getDefinition().getLabel());
+		assertEquals("|", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006continuing06();
+		assertEquals("Form of item", value.getDefinition().getLabel());
+		assertEquals(" ", value.getValue());
+		assertEquals("None of the following", value.resolve());
+
+		value = field.getTag006continuing07();
+		assertEquals("Nature of entire work", value.getDefinition().getLabel());
+		assertEquals(" ", value.getValue());
+		assertEquals("Not specified", value.resolve());
+
+		value = field.getTag006continuing08();
+		assertEquals("Nature of contents", value.getDefinition().getLabel());
+		assertEquals("   ", value.getValue());
+		assertEquals("Not specified", value.resolve());
+
+		value = field.getTag006continuing11();
+		assertEquals("Government publication", value.getDefinition().getLabel());
+		assertEquals(" ", value.getValue());
+		assertEquals("Not a government publication", value.resolve());
+
+		value = field.getTag006continuing12();
+		assertEquals("Conference publication", value.getDefinition().getLabel());
+		assertEquals("0", value.getValue());
+		assertEquals("Not a conference publication", value.resolve());
+
+		value = field.getTag006continuing16();
+		assertEquals("Original alphabet or script of title", value.getDefinition().getLabel());
+		assertEquals("b", value.getValue());
+		assertEquals("Extended Roman", value.resolve());
+
+		value = field.getTag006continuing17();
+		assertEquals("Entry convention", value.getDefinition().getLabel());
+		assertEquals("0", value.getValue());
+		assertEquals("Successive entry", value.resolve());
+	}
+
+	@Test
+	public void testVisualMaterialsGetters() {
+		Control006 field = new Control006("r|||            ou", Leader.Type.VISUAL_MATERIALS);
+
+		ControlValue value = field.getTag006all00();
+		assertEquals("Form of material", value.getDefinition().getLabel());
+		assertEquals("r", value.getValue());
+		assertEquals("Three-dimensional artifact or naturally occurring object", value.resolve());
+
+		value = field.getTag006visual01();
+		assertEquals("Running time for motion pictures and videorecordings", value.getDefinition().getLabel());
+		assertEquals("|||", value.getValue());
+		assertEquals("No attempt to code", value.resolve());
+
+		value = field.getTag006visual05();
+		assertEquals("Target audience", value.getDefinition().getLabel());
+		assertEquals(" ", value.getValue());
+		assertEquals("Unknown or not specified", value.resolve());
+
+		value = field.getTag006visual11();
+		assertEquals("Government publication", value.getDefinition().getLabel());
+		assertEquals(" ", value.getValue());
+		assertEquals("Not a government publication", value.resolve());
+
+		value = field.getTag006visual12();
+		assertEquals("Form of item", value.getDefinition().getLabel());
+		assertEquals(" ", value.getValue());
+		assertEquals("None of the following", value.resolve());
+
+		value = field.getTag006visual16();
+		assertEquals("Type of visual material", value.getDefinition().getLabel());
+		assertEquals("o", value.getValue());
+		assertEquals("Flash card", value.resolve());
+
+		value = field.getTag006visual17();
+		assertEquals("Technique", value.getDefinition().getLabel());
+		assertEquals("u", value.getValue());
+		assertEquals("Unknown", value.resolve());
+	}
+
 }

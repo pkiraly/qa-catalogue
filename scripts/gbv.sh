b50897887af235835239096f4e06a5a55de42649
pkiraly <pkiraly@gwdg.de>
2019-11-11 08:55:16 +0100

    Adding GBV script

---
+++ scripts/gbv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+. ./setdir.sh
+NAME=gbv
+# TYPE_PARAMS="--marcVersion SZTE"
+MARC_DIR=${BASE_INPUT_DIR}/gbv/2019-10-01
+MASK=*.mrc
+
+. ./common-script
+
+echo "DONE"
+exit 0

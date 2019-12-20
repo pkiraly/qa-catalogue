b31e9bd23973208d0fdd1f0f88996246db884089
Peter Kiraly <peter.kiraly@gwdg.de>
2019-12-20 17:09:18 +0100

    Creating pairs from groups in network analysis.

--- src/main/java/de/gwdg/metadataqa/marc/cli/NetworkAnalysis.java
+++ src/main/java/de/gwdg/metadataqa/marc/cli/NetworkAnalysis.java
@@ -4,11 +4,13 @@ import de.gwdg.metadataqa.marc.DataField;
 import de.gwdg.metadataqa.marc.MarcRecord;
 import de.gwdg.metadataqa.marc.analysis.NetworkAnalyzer;
 import de.gwdg.metadataqa.marc.cli.parameters.CommonParameters;
-import de.gwdg.metadataqa.marc.cli.parameters.ValidatorParameters;
+import de.gwdg.metadataqa.marc.cli.parameters.NetworkAction;
+import de.gwdg.metadataqa.marc.cli.parameters.NetworkParameters;
 import de.gwdg.metadataqa.marc.cli.processor.MarcFileProcessor;
 import de.gwdg.metadataqa.marc.cli.utils.RecordIterator;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
 import org.marc4j.marc.Record;
 
 import java.io.BufferedWriter;
@@ -18,7 +20,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import static de.gwdg.metadataqa.marc.Utils.createRow;
 
@@ -26,16 +30,15 @@ public class NetworkAnalysis implements MarcFileProcessor, Serializable {
 
   private static final Logger logger = Logger.getLogger(NetworkAnalysis.class.getCanonicalName());
 
-  private final ValidatorParameters parameters;
+  private final NetworkParameters parameters;
   private final Options options;
   private final boolean readyToProcess;
-  // private final Map<DataField, List<Integer>> collector = new HashMap<>();
   private final List<String> orphans = new ArrayList<>();
   private Path path;
   private BufferedWriter writer;
 
   public NetworkAnalysis(String[] args) throws ParseException {
-    parameters = new ValidatorParameters(args);
+    parameters = new NetworkParameters(args);
     options = parameters.getOptions();
     readyToProcess = true;
   }
@@ -49,17 +52,81 @@ public class NetworkAnalysis implements MarcFileProcessor, Serializable {
       // processor.printHelp(processor.getParameters().getOptions());
       System.exit(0);
     }
-    if (processor.getParameters().getArgs().length < 1) {
-      System.err.println("Please provide a MARC file name!");
-      processor.printHelp(processor.getParameters().getOptions());
-      System.exit(0);
+    logger.info("Action: " + ((NetworkParameters)processor.getParameters()).getAction());
+    if (((NetworkParameters)processor.getParameters()).getAction().equals(NetworkAction.PAIRING)) {
+      ((NetworkAnalysis)processor).pairIds();
+    } else {
+      if (processor.getParameters().getArgs().length < 1) {
+        System.err.println("Please provide a MARC file name!");
+        processor.printHelp(processor.getParameters().getOptions());
+        System.exit(0);
+      }
+      if (processor.getParameters().doHelp()) {
+        processor.printHelp(processor.getParameters().getOptions());
+        System.exit(0);
+      }
+      RecordIterator iterator = new RecordIterator(processor);
+      iterator.start();
     }
-    if (processor.getParameters().doHelp()) {
-      processor.printHelp(processor.getParameters().getOptions());
-      System.exit(0);
+  }
+
+  private void pairIds() {
+    logger.info("pairIds");
+    Path outputPath = Paths.get(parameters.getOutputDir(), "network-pairs.csv");
+    try {
+      writer = Files.newBufferedWriter(outputPath);
+      writer.write(createRow("id1", "id2"));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    RecordIterator iterator = new RecordIterator(processor);
-    iterator.start();
+
+    AtomicInteger lineNr = new AtomicInteger();
+    Path inputPath = Paths.get(parameters.getOutputDir(), "network-by-concepts.csv");
+    try (Stream<String> stream = Files.lines(Paths.get(inputPath.toString()))) {
+      stream.forEach(
+        line -> {
+          lineNr.getAndIncrement();
+          if (lineNr.get() % 100 == 0)
+            logger.info("" + lineNr.get());
+          String[] parts = line.split(",");
+          String[] ids = parts[2].split(";");
+          if (ids.length > 1) {
+            if (ids.length > parameters.getGroupLimit()) {
+              logger.info(String.format("%d is greater than %d. The array will will truncated.", ids.length, parameters.getGroupLimit()));
+              ids = Arrays.copyOfRange(ids, 0, parameters.getGroupLimit());
+            }
+
+            String[] encoded = new String[ids.length];
+            for (int i = 0; i<ids.length; i++) {
+              encoded[i] = encode(ids[i]);
+            }
+            for (int i = 0; i < encoded.length-1; i++) {
+              for (int j = (i+1); j < encoded.length; j++) {
+                try {
+                  writer.write(createRow(encoded[i], encoded[j]));
+                } catch (IOException e) {
+                  e.printStackTrace();
+                }
+              }
+            }
+          }
+        }
+      );
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private String encode(String id) {
+    if (id.contains("+"))
+      return id;
+    else
+      return Integer.toString(Integer.parseInt(id), Character.MAX_RADIX);
   }
 
   @Override
---
+++ src/main/java/de/gwdg/metadataqa/marc/cli/parameters/NetworkParameters.java
@@ -0,0 +1,60 @@
+package de.gwdg.metadataqa.marc.cli.parameters;
+
+import de.gwdg.metadataqa.marc.model.validation.ValidationErrorFormat;
+import org.apache.commons.cli.ParseException;
+
+import java.io.Serializable;
+
+public class NetworkParameters extends CommonParameters implements Serializable {
+  public static final String DEFAULT_FILE_NAME = "validation-report.txt";
+
+  private NetworkAction action = NetworkAction.PRIMARY;
+  private int groupLimit = 1000;
+  private boolean isOptionSet;
+
+  protected void setOptions() {
+    if (!isOptionSet) {
+      super.setOptions();
+      options.addOption("a", "action", true, "action: 'primary' (default), 'pairing'");
+      options.addOption("l", "group-limit", true, "pair creation limit");
+      isOptionSet = true;
+    }
+  }
+
+  public NetworkParameters() {
+    super();
+  }
+
+  public NetworkParameters(String[] arguments) throws ParseException {
+    super(arguments);
+
+    if (cmd.hasOption("action")) {
+      for (NetworkAction registeredAction : NetworkAction.values()) {
+        if (registeredAction.getLabel().equals(cmd.getOptionValue("action"))) {
+          action = registeredAction;
+          break;
+        }
+      }
+    }
+
+    if (cmd.hasOption("group-limit")) {
+      groupLimit = Integer.parseInt(cmd.getOptionValue("group-limit"));
+    }
+  }
+
+  public NetworkAction getAction() {
+    return action;
+  }
+
+  public int getGroupLimit() {
+    return groupLimit;
+  }
+
+  @Override
+  public String formatParameters() {
+    String text = super.formatParameters();
+    text += String.format("action: %s%n", action);
+    text += String.format("group-limit: %d%n", groupLimit);
+    return text;
+  }
+}

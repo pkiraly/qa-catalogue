bc2eaf8c4850b44b9e4b876c950c88811062b1f8
chlad.josef <chlad.josef@svkpk.cz>
2020-04-14 18:02:58 +0200

    NKCR 903 added

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/nkcrtags/Tag903.java
@@ -0,0 +1,36 @@
+package de.gwdg.metadataqa.marc.definition.tags.nkcrtags;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.Indicator;
+
+public class Tag903 extends DataFieldDefinition {
+
+  private static Tag903 uniqueInstance;
+
+  private Tag903() {
+    initialize();
+    postCreation();
+  }
+
+  public static Tag903 getInstance() {
+    if (uniqueInstance == null)
+      uniqueInstance = new Tag903();
+    return uniqueInstance;
+  }
+
+  private void initialize() {
+    tag = "903";
+    label = "Označení - staré tisky";
+    cardinality = Cardinality.Nonrepeatable;
+    descriptionUrl = "https://www.nkp.cz/o-knihovne/odborne-cinnosti/zpracovani-fondu/informativni-materialy/blok-9xxuni-m21";
+    setCompilanceLevels("A", "A");
+
+    ind1 = new Indicator();
+    ind2 = new Indicator();
+
+    setSubfieldsWithCardinality(
+      "a", "Označení \"RP\"", "NR"
+    );
+  }
+}

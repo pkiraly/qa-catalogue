c40f1dca3a2cdc4cbf0465857bb15c966613bb6c
Radek <radek.svetlik2@gmail.com>
2020-04-14 17:02:11 +0200

    Add NKCR version settings

--- src/main/java/de/gwdg/metadataqa/marc/definition/TagDefinitionLoader.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/TagDefinitionLoader.java
@@ -74,6 +74,8 @@ public class TagDefinitionLoader {
       version = MarcVersion.SZTE;
     } else if (definitionClazz.getCanonicalName().contains(".fennicatags.")) {
       version = MarcVersion.FENNICA;
+    } else if (definitionClazz.getCanonicalName().contains(".nkcrtags.")) {
+      version = MarcVersion.NKCR;
     }
     return version;
   }

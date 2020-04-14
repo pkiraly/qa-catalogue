22df5a584b11d2d756ccc67cb00b88feee0c15cc
chlad.josef <chlad.josef@svkpk.cz>
2020-04-14 17:56:42 +0200

    NKCR tags 900, 901, 902 added

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/nkcrtags/Tag900.java
@@ -0,0 +1,37 @@
+package de.gwdg.metadataqa.marc.definition.tags.nkcrtags;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.Indicator;
+
+public class Tag900 extends DataFieldDefinition {
+
+  private static Tag900 uniqueInstance;
+
+  private Tag900() {
+    initialize();
+    postCreation();
+  }
+
+  public static Tag900 getInstance() {
+    if (uniqueInstance == null)
+      uniqueInstance = new Tag900();
+    return uniqueInstance;
+  }
+
+  private void initialize() {
+    tag = "900";
+    label = "Vlastník a úroveň zpracování záznamu";
+    cardinality = Cardinality.Nonrepeatable;
+    descriptionUrl = "https://www.nkp.cz/o-knihovne/odborne-cinnosti/zpracovani-fondu/informativni-materialy/blok-9xxuni-m21";
+    setCompilanceLevels("A", "A");
+
+    ind1 = new Indicator();
+    ind2 = new Indicator();
+
+    setSubfieldsWithCardinality(
+      "a", "Sigla vlastníka záznamu (zasílající instituce)", "NR",
+      "b", "Váha záznamu", "NR"
+    );
+  }
+}
---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/nkcrtags/Tag902.java
@@ -0,0 +1,57 @@
+package de.gwdg.metadataqa.marc.definition.tags.nkcrtags;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.Indicator;
+import de.gwdg.metadataqa.marc.definition.general.validator.ISBNValidator;
+import static de.gwdg.metadataqa.marc.definition.FRBRFunction.*;
+
+public class Tag902 extends DataFieldDefinition {
+  private static Tag902 uniqueInstance;
+
+  private Tag902() {
+    initialize();
+    postCreation();
+  }
+
+  public static Tag902 getInstance() {
+    if (uniqueInstance == null)
+      uniqueInstance = new Tag902();
+    return uniqueInstance;
+  }
+
+  private void initialize() {
+    tag = "902";
+    label = "Nadstandardní ISBN (u seriálů)";
+    cardinality = Cardinality.Repeatable;
+    descriptionUrl = "https://www.nkp.cz/o-knihovne/odborne-cinnosti/zpracovani-fondu/informativni-materialy/blok-9xxuni-m21";
+    setCompilanceLevels("O");
+
+    ind1 = new Indicator();
+    ind2 = new Indicator();
+
+    setSubfieldsWithCardinality(
+      "a", "International Standard Book Number", "NR",
+      "c", "Terms of availability", "NR",
+      "q", "Qualifying information", "R",
+      "z", "Canceled/invalid ISBN", "R"
+    );
+
+    getSubfield("a").setValidator(ISBNValidator.getInstance());
+
+    getSubfield("a")
+      .setBibframeTag("rdf:value")
+      .setFrbrFunctions(DiscoverySearch, DiscoveryIdentify, DiscoveryObtain);
+
+    getSubfield("c")
+      .setBibframeTag("acquisitionTerms")
+      .setFrbrFunctions(DiscoverySelect, DiscoveryObtain);
+
+    getSubfield("q")
+      .setBibframeTag("qualifier");
+
+    getSubfield("z")
+      .setMqTag("canceledOrInvalidISBN")
+      .setFrbrFunctions(DiscoverySearch, DiscoveryIdentify, DiscoveryObtain);
+  }
+}

acde3980f6c76d84271205f8f0dd0ba2dc3eb00a
Peter Kiraly <peter.kiraly@gwdg.de>
2020-05-27 17:51:28 +0200

    Improving parameters in Gent script

--- scripts/gent.sh
+++ scripts/gent.sh
@@ -2,9 +2,12 @@
 
 . ./setdir.sh
 NAME=gent
-TYPE_PARAMS="--marcVersion GENT"
-MARC_DIR=${BASE_INPUT_DIR}/gent/marc/2019-06-05
-MASK=*.mrc
+# TYPE_PARAMS="--marcVersion GENT"
+# MARC_DIR=${BASE_INPUT_DIR}/gent/marc/2019-06-05
+# MASK=*.mrc
+TYPE_PARAMS="--marcVersion GENT --alephseq"
+MARC_DIR=${BASE_INPUT_DIR}/gent/marc/2020-05-27
+MASK=*.export
 
 . ./common-script
 

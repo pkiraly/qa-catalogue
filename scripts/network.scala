e206205fa030ef50ec13a2690b50dacfaea85880
Peter Kiraly <peter.kiraly@gwdg.de>
2020-10-28 10:24:55 +0100

    Making modular

--- scripts/network.scala
+++ scripts/network.scala
@@ -7,17 +7,25 @@ import scala.collection.JavaConverters._
 val log = Logger.getLogger("network-analysis")
 log.setLevel(Level.INFO)
 
-val dir = "file://" + sc.getConf.get("spark.driver.metadata.qa.dir") + "/"
-log.info(dir)
+var inputDir = "file://" + sc.getConf.get("spark.driver.metadata.qa.inputDir")
+if (!inputDir.endsWith("/"))
+  inputDir = inputDir + "/"
+
+var outputDir = "file://" + sc.getConf.get("spark.driver.metadata.qa.outputDir")
+if (!outputDir.endsWith("/"))
+  outputDir = outputDir + "/"
+
+log.info(s"inputDir: $inputDir")
+log.info(s"outputDir: $outputDir")
 
 def write(file: String, df: DataFrame): Unit = {
   var dataDF = df.select(df.columns.map(c => df.col(c).cast("string")): _*)
   var headerDF = spark.createDataFrame(List(Row.fromSeq(dataDF.columns.toSeq)).asJava, dataDF.schema)
-  var outputFolder = dir + file + ".csv.dir"
+  var outputFolder = outputDir + file + ".csv.dir"
   headerDF.union(dataDF).write.option("header", "false").mode(SaveMode.Overwrite).csv(outputFolder)
 }
 
-def pageRank(graph: Graph[Array[String],Int]): Unit = {
+def pageRank(graph: Graph[Array[String],Int], suffix: String): Unit = {
   log.info("STEP 1: page rank")
 
   var pagerankGraph = graph.pageRank(0.001)
@@ -29,50 +37,52 @@ def pageRank(graph: Graph[Array[String],Int]): Unit = {
   }
 
   var df_pagerank = infoWithPageRank.vertices.map(e => (e._1, e._2._1)).toDF("id", "score")
-  write("network-nodes-pagerank", df_pagerank)
+  write("network-scores" + suffix + "-pagerank", df_pagerank)
 
   // page rank stat
   var dataDF = df_pagerank.select("score").summary().toDF("statistic", "value")
-  write("network-nodes-pagerank-stat", dataDF)
+  write("network-scores" + suffix + "-pagerank-stat", dataDF)
 
   // page rank histogram
   var histogram = df_pagerank.select(round($"score", 0).as("score").cast("int")).groupBy("score").count().orderBy("score")
-  write("network-nodes-pagerank-histogram", histogram)
+  write("network-scores" + suffix + "-pagerank-histogram", histogram)
 }
 
-def connectedComponents(graph: Graph[Array[String],Int]): Unit = {
+def connectedComponents(graph: Graph[Array[String],Int], suffix: String): Unit = {
   log.info("STEP 2: connectedComponents")
 
   var cc = graph.connectedComponents()
   var componentDF = cc.vertices.toDF("vid", "cid")
   var componentsCount = componentDF.groupBy("cid").count().toDF("componentId", "size")
-  write("network-nodes-components", componentsCount.orderBy(desc("count")))
+  write("network-scores" + suffix + "-components", componentsCount.orderBy(desc("count")))
 
   // connectedComponents stat
   var statDF = componentsCount.select("size").summary().toDF("statistic", "value")
-  write("network-nodes-components-stat", statDF)
+  write("network-scores" + suffix + "-components-stat", statDF)
 
   var histogram = componentsCount.select("size").groupBy("size").count().orderBy("size")
-  write("network-nodes-components-histogram", histogram)
+  write("network-scores" + suffix + "-components-histogram", histogram)
 }  
 
-def degree(graph: Graph[Array[String],Int]): Unit = {
+def degree(graph: Graph[Array[String],Int], suffix: String): Unit = {
   log.info("STEP 3: degree")
 
   var degreesRDD = graph.degrees.cache()
   var df = degreesRDD.toDF("id", "degree")
-  write("network-nodes-degrees", df.orderBy(desc("degree")))
+  write("network-scores" + suffix + "-degrees", df.orderBy(desc("degree")))
 
   var dataDF = df.select("degree").summary().toDF("statistic", "value")
-  write("network-nodes-degrees-stat", dataDF)
+  write("network-scores" + suffix + "-degrees-stat", dataDF)
 
   var histogram = df.select("degree").groupBy("degree").count().orderBy("degree")
-  write("network-nodes-degrees-histogram", histogram)
+  write("network-scores" + suffix + "-degrees-histogram", histogram)
 }
 
-def analyseGraph(fileName: String, nodes: RDD[(Long, Array[String])]): Unit = {
+def analyseGraph(prefix: String, suffix: String, nodes: RDD[(Long, Array[String])]): Unit = {
   // Parse the edge data which is already in userId -> userId format
-  val followerGraph: Graph[Int,Int] = GraphLoader.edgeListFile(sc, dir + fileName)
+  log.info("create graph")
+  val file = prefix + suffix + ".csv";
+  val followerGraph: Graph[Int,Int] = GraphLoader.edgeListFile(sc, inputDir + file)
 
   // Attach the attributes
   val graph: Graph[Array[String],Int] = followerGraph.outerJoinVertices(nodes) {
@@ -80,46 +90,42 @@ def analyseGraph(fileName: String, nodes: RDD[(Long, Array[String])]): Unit = {
     // Some nodes may not have attributes so we set them as empty
     case (uid, deg, None) => Array.empty[String]
   }
+  graph.cache()
+  // graph.count()
 
-  pageRank(graph)
-  connectedComponents(graph)
-  degree(graph)
+  pageRank(graph, suffix)
+  connectedComponents(graph, suffix)
+  degree(graph, suffix)
 }
 
 // val dir = "file:///home/kiru/bin/marc/_output/gent/"
 log.info("prepare")
 
 // Load my user data and parse into tuples of user id and attribute list
-val nodes: RDD[(Long, Array[String])] = (sc.textFile(dir + "network-nodes.csv")
+val nodes: RDD[(Long, Array[String])] = (sc.textFile(inputDir + "network-nodes.csv")
   .map(line => line.split(","))
   .map(parts => (parts.head.toLong, parts.tail)))
+nodes.cache()
+nodes.count
 
-analyseGraph("network-pairs.csv", nodes)
+// analyseGraph("network-pairs", "", nodes)
 
-
-/*
-// STEP 5: clustering coefficients
-log.info("STEP 5: clustering coefficients")
-
-val triCountGraph = graph.triangleCount()
-triCountGraph.vertices.map(x => x._2).stats()
-val tricountDF = triCountGraph.vertices.toDF("id", "count")
-
-val maxTrisGraph = degreesRDD.mapValues(d => d * (d - 1) / 2.0)
-val maxTrisDF = maxTrisGraph.toDF("id", "theoreticalMax")
-
-val clusterCoef = triCountGraph.vertices.innerJoin(maxTrisGraph) { 
-  (vertexId, triCount, maxTris) => {
-    val coef = if (maxTris == 0) 0 else triCount / maxTris
-    (triCount, maxTris, coef)
-  }
+val tagsDF = spark.read.
+                  option("header", true).
+                  option("inferSchema", "false").
+                  csv(inputDir + "network-by-concepts-tags.csv")
+val tags = tagsDF.select($"tag".cast("String")).
+               rdd.
+               map(r => r(0).asInstanceOf[String]).
+               collect()
+val allTags = Array("all") ++ tags
+for (i <- allTags.indices) {
+  val tag = allTags(i)
+  val suffix = "-" + tag
+  log.info(s"==== [ ${i+1}/${tags.length}: $tag ] ====")
+  analyseGraph("network-pairs", suffix, nodes)
 }
-val networkAverageClusteringCoefficient = clusterCoef.map(_._2._3).sum() / graph.vertices.count()
 
-val naccDF = sc.parallelize(Seq(networkAverageClusteringCoefficient)).toDF("average-clustering-coefficient")
-var outputFolder = dir + "network-nodes-average-clustering-coefficient.csv.dir"
-naccDF.write.option("header", "true").mode(SaveMode.Overwrite).csv(outputFolder)
-*/
 
 log.info("DONE")
 

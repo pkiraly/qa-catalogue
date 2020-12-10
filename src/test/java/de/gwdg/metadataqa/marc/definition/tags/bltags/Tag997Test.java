3a9a0698666b4da0855a3f1c6d42ae7bce7cab23
Peter Kiraly <peter.kiraly@gwdg.de>
2020-12-10 17:41:24 +0100

    issue #75: add tag 997

---
+++ src/test/java/de/gwdg/metadataqa/marc/definition/tags/bltags/Tag997Test.java
@@ -0,0 +1,24 @@
+package de.gwdg.metadataqa.marc.definition.tags.bltags;
+
+import de.gwdg.metadataqa.marc.DataField;
+import org.junit.Test;
+
+public class Tag997Test extends BLTagTest {
+
+  public Tag997Test() {
+    super(Tag997.getInstance());
+  }
+
+  @Test
+  public void testValidFields() {
+    validField("a", "100: another author?");
+    validField("a", "100: another author?", "a", "another message");
+  }
+
+  @Test
+  public void testInvalidFields() {
+    invalidField("2", "a", "Priority processing");
+    invalidField("5", "B");
+    invalidField("b", "Card,");
+  }
+}

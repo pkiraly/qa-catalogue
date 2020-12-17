04075a5f0f68fc25c8e2a7aa1233fa0fed1f1c21
Peter Kiraly <peter.kiraly@gwdg.de>
2020-12-17 12:27:00 +0100

    issue #75: add tag CNF

---
+++ src/test/java/de/gwdg/metadataqa/marc/definition/tags/bltags/TagCNFTest.java
@@ -0,0 +1,27 @@
+package de.gwdg.metadataqa.marc.definition.tags.bltags;
+
+import de.gwdg.metadataqa.marc.DataField;
+import org.junit.Test;
+
+public class TagCNFTest extends BLTagTest {
+
+  public TagCNFTest() {
+    super(TagCNF.getInstance());
+  }
+
+  @Test
+  public void testValidFields() {
+    validField("a", "Academy of Marketing Science (Annual Conference)", "d", "(1997 May :", "c", "Coral Gables, FL)");
+    validField("a", "Database and expert systems applications (International conference)", "n", "(10th :", "d", "1999 Aug :", "c", "Florence, Italy)");
+    validField("a", "Algebraic and combinatorial approaches to representation theory (Satellite conference)", "d", "(2010 Aug :", "c", "Bangalore, India)");
+    validField("a", "Administrative Sciences Association of Canada.", "e", "Tourism and Hospitality Management Division (Annual conference)", "d", "(2012 May :", "c", "St John's, Canada)");
+    validField("a", "Advanced materials engineering and technology (International conference)", "n", "(2nd :", "d", "2013 Nov :", "c", "Bandung, Indonesia)");
+  }
+
+  @Test
+  public void testInvalidFields() {
+    invalidField("3", "a", "t");
+    invalidField("h", "B");
+    invalidField("j", "20201236");
+  }
+}

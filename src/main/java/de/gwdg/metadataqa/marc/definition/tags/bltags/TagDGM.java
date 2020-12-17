b5f5b7bcccc7658280b6d189b94dd0331eafb0d0
Peter Kiraly <peter.kiraly@gwdg.de>
2020-12-17 15:10:56 +0100

    issue #75: add tag DGM

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/bltags/TagDGM.java
@@ -0,0 +1,47 @@
+package de.gwdg.metadataqa.marc.definition.tags.bltags;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.Indicator;
+import de.gwdg.metadataqa.marc.definition.general.validator.RegexValidator;
+
+/**
+ * Digitised Record Match
+ */
+public class TagDGM extends DataFieldDefinition {
+
+  private static TagDGM uniqueInstance;
+
+  private TagDGM() {
+    initialize();
+    postCreation();
+  }
+
+  public static TagDGM getInstance() {
+    if (uniqueInstance == null)
+      uniqueInstance = new TagDGM();
+    return uniqueInstance;
+  }
+
+  private void initialize() {
+
+    tag = "DGM";
+    label = "Digitised Record Match";
+    mqTag = "digitisedRecordMatch";
+    cardinality = Cardinality.Nonrepeatable;
+    // descriptionUrl = "https://www.loc.gov/marc/bibliographic/bd037.html";
+    // setCompilanceLevels("O");
+
+    ind1 = new Indicator();
+
+    ind2 = new Indicator();
+
+    setSubfieldsWithCardinality(
+      "a", "System number", "NR"
+    );
+
+    getSubfield("a")
+      .setValidator(new RegexValidator("^.*[^\\.]$"))
+      .setMqTag("systemNumber");
+  }
+}

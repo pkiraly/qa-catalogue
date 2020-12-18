a7009ebf396340ab9921038750a8405765a652a7
Peter Kiraly <peter.kiraly@gwdg.de>
2020-12-18 22:32:55 +0100

    issue #75: add tag RSC

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/bltags/TagRSC.java
@@ -0,0 +1,47 @@
+package de.gwdg.metadataqa.marc.definition.tags.bltags;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.Indicator;
+import de.gwdg.metadataqa.marc.definition.general.validator.RegexValidator;
+
+/**
+ * Remote Supply Collection
+ */
+public class TagRSC extends DataFieldDefinition {
+
+  private static TagRSC uniqueInstance;
+
+  private TagRSC() {
+    initialize();
+    postCreation();
+  }
+
+  public static TagRSC getInstance() {
+    if (uniqueInstance == null)
+      uniqueInstance = new TagRSC();
+    return uniqueInstance;
+  }
+
+  private void initialize() {
+
+    tag = "RSC";
+    label = "Remote Supply Collection";
+    mqTag = "RemoteSupplyCollection";
+    cardinality = Cardinality.Nonrepeatable;
+    // descriptionUrl = "https://www.loc.gov/marc/bibliographic/bd037.html";
+    // setCompilanceLevels("O");
+
+    ind1 = new Indicator();
+
+    ind2 = new Indicator();
+
+    setSubfieldsWithCardinality(
+      "a", "Code", "NR"
+    );
+
+    getSubfield("a")
+      .setCodes("Y", "Y")
+      .setMqTag("code");
+  }
+}

ccf11f3f23fda9506a00d0772a67d8f26b8f9bb4
pkiraly <pkiraly@gwdg.de>
2021-08-25 16:36:38 +0200

    Implementing a tag set for the University of Amsterdam library catalogue

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/uvatags/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains the tags specifics at library of the University of Amsterdam (The Netherlands)
+ *
+ * The source of the classes is this document is an Excel file I received from from Hadewijch Dekker <h.e.dekker@uva.nl>
+ */
+package de.gwdg.metadataqa.marc.definition.tags.uvatags;
+
+//rest of the file is empty

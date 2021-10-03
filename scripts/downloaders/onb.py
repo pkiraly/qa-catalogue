ce48c5b49b21b685f6115aec2997d4df44089cd2
Peter Kiraly <peter.kiraly@gwdg.de>
2021-10-03 11:29:13 +0200

    Add downloader for ÖNB

---
+++ scripts/downloaders/onb.py
@@ -0,0 +1,31 @@
+from lxml import etree
+import io
+from sickle import Sickle
+
+namespaces = {
+    'oai': 'http://www.openarchives.org/OAI/2.0/',
+    'marc21': 'http://www.loc.gov/MARC21/slim'
+} 
+
+sickle = Sickle('https://eu02.alma.exlibrisgroup.com/view/oai/43ACC_ONB/request')
+
+header = '<?xml version="1.0" encoding="utf8"?>' + "\n" + '<records>' + "\n" 
+footer = '</records>'
+
+output = []
+i = 0
+it = sickle.ListRecords(metadataPrefix='marc21', set='FULLMARC')
+for record in it:
+    tree = etree.ElementTree(record.xml)
+    recs = tree.xpath('/oai:record[*]/oai:metadata/marc21:record', namespaces=namespaces)
+    for rec in recs:
+        core = etree.tostring(rec, encoding='utf8', method='xml').decode("utf-8")
+        output.append(core.replace("<?xml version='1.0' encoding='utf8'?>\n", ''))
+    if len(output) >= 1000:
+        file_name = "%06d.xml" % (i) 
+        with io.open(file_name, 'w', encoding='utf8') as f:
+            f.write(header)
+            f.write("\n".join(output) + "\n")
+            f.write(footer)
+        output = []
+        i = i + 1

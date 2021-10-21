9c80fe72313677cfaa3c1004062604e842b3263e
pkiraly <pkiraly@gwdg.de>
2021-10-21 13:50:14 +0200

    Introducing SQLite.

--- scripts/r-scripts/create-database.R
+++ scripts/r-scripts/create-database.R
@@ -14,28 +14,32 @@ if (length(args) == 0) {
 my_db_file <- paste0(dir, "/qa_catalogue.sqlite")
 con <- DBI::dbConnect(RSQLite::SQLite(), my_db_file)
 
+print('transforming issue summary')
 issue_summary <- read_csv(paste0(dir, '/issue-summary.csv'), 
                           col_types = 'iciicccii')
 copy_to(con, issue_summary, overwrite = TRUE, temporary = FALSE)
 
-df <- read_csv(paste0(dir, '/issue-details.csv'), col_types = 'cc')
+#' transforming issue details
+print('transforming issue details')
+issue_details <- read_csv(paste0(dir, '/issue-details-normalized.csv'), col_types = 'cii')
 
-issue_details <- df %>% 
-  separate(errors, LETTERS, sep = ';', fill="right") %>% 
-  pivot_longer(
-    all_of(LETTERS),
-    names_to = 'key',
-    values_to = 'value',
-    values_drop_na = TRUE
-  ) %>% 
-  select(recordId, value) %>% 
-  separate(value, c('errorId', 'instances'), sep=':', fill='right') %>% 
-  mutate(
-    errorId = as.integer(errorId),
-    instances = as.integer(instances)
-  )
+# df <- read_csv(paste0(dir, '/issue-details.csv'), col_types = 'cc')
+
+# issue_details <- df %>% 
+#   separate(errors, LETTERS, sep = ';', fill="right") %>% 
+#   pivot_longer(
+#     all_of(LETTERS),
+#     names_to = 'key',
+#     values_to = 'value',
+#     values_drop_na = TRUE
+#   ) %>% 
+#   select(recordId, value) %>% 
+#   separate(value, c('errorId', 'instances'), sep=':', fill='right') %>% 
+#   mutate(
+#     errorId = as.integer(errorId),
+#     instances = as.integer(instances)
+#   )
 copy_to(con, issue_details, overwrite = TRUE, temporary = FALSE)
 
 DBI::dbListTables(con)
-
 DBI::dbDisconnect(con)
--- scripts/r-scripts/create-indices.sql
+++ scripts/r-scripts/create-indices.sql
@@ -1,4 +1,4 @@
-CREATE INDEX "errorId" ON "issue_details" ("errorId")
-CREATE INDEX "id" ON "issue_summary" ("id")
-CREATE INDEX "categoryId" ON "issue_summary" ("categoryId")
-CREATE INDEX "typeId" ON "issue_summary" ("typeId")
+CREATE INDEX "errorId" ON "issue_details" ("errorId");
+CREATE INDEX "id" ON "issue_summary" ("id");
+CREATE INDEX "categoryId" ON "issue_summary" ("categoryId");
+CREATE INDEX "typeId" ON "issue_summary" ("typeId");

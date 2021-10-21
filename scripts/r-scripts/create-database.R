7bc5b05a8cacb6b5ee7f43c618d5b13348349280
pkiraly <pkiraly@gwdg.de>
2021-10-21 10:42:48 +0200

    Improve SQLite procedures.

--- scripts/r-scripts/create-database.R
+++ scripts/r-scripts/create-database.R
@@ -11,15 +11,31 @@ if (length(args) == 0) {
   dir <- args[1]
 }
 
-issue_summary <- read_csv(paste0(dir, '/issue-summary.csv'), 
-                          col_types = 'iciicccii')
-
-str(issue_summary)
-
 my_db_file <- paste0(dir, "/qa_catalogue.sqlite")
 con <- DBI::dbConnect(RSQLite::SQLite(), my_db_file)
 
+issue_summary <- read_csv(paste0(dir, '/issue-summary.csv'), 
+                          col_types = 'iciicccii')
 copy_to(con, issue_summary, overwrite = TRUE, temporary = FALSE)
+
+df <- read_csv(paste0(dir, '/issue-details.csv'), col_types = 'cc')
+
+issue_details <- df %>% 
+  separate(errors, LETTERS, sep = ';', fill="right") %>% 
+  pivot_longer(
+    all_of(LETTERS),
+    names_to = 'key',
+    values_to = 'value',
+    values_drop_na = TRUE
+  ) %>% 
+  select(recordId, value) %>% 
+  separate(value, c('errorId', 'instances'), sep=':', fill='right') %>% 
+  mutate(
+    errorId = as.integer(errorId),
+    instances = as.integer(instances)
+  )
+copy_to(con, issue_details, overwrite = TRUE, temporary = FALSE)
+
 DBI::dbListTables(con)
 
 DBI::dbDisconnect(con)

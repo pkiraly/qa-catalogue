0081166de0abf5b9d7752ee8b9ebb70ec282340b
pkiraly <pkiraly@gwdg.de>
2021-10-22 15:18:38 +0200

    Delete renamed file.

--- scripts/r-scripts/create-indices.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX "errorId" ON "issue_details" ("errorId");
-CREATE INDEX "id" ON "issue_summary" ("id");
-CREATE INDEX "categoryId" ON "issue_summary" ("categoryId");
-CREATE INDEX "typeId" ON "issue_summary" ("typeId");

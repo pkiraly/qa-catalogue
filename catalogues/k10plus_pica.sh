9c877b771487a70e08830d2e96389756f530cc5d
pkiraly <pkiraly@gwdg.de>
2022-06-17 19:52:53 +0200

    issue #144: Filter out internal fields

--- catalogues/k10plus_pica.sh
+++ catalogues/k10plus_pica.sh
@@ -3,7 +3,7 @@
 . ./setdir.sh
 NAME=k10plus_pica
 MARC_DIR=${BASE_INPUT_DIR}/k10plus_pica
-TYPE_PARAMS="--schemaType PICA --marcFormat PICA_PLAIN --emptyLargeCollectors"
+TYPE_PARAMS="--schemaType PICA --marcFormat PICA_PLAIN --emptyLargeCollectors --ignorableFields 001@,001E,001L,001U,001U,001X,001X,002V,003C,003G,003Z,008G,017N,020F,027D,031B,037I,039V,042@,046G,046T"
 MASK=sample.pica
 
 . ./common-script

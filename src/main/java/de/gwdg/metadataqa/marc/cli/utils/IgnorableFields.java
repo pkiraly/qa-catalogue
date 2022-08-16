7076bd8fce4fbbc68432c3e08f146953c4d5cee4
Péter Király <pkiraly@gwdg.de>
2022-08-16 16:23:09 +0200

    issue #152: ignorableFields parameter should support masking

--- src/main/java/de/gwdg/metadataqa/marc/cli/utils/IgnorableFields.java
+++ src/main/java/de/gwdg/metadataqa/marc/cli/utils/IgnorableFields.java
@@ -2,15 +2,27 @@ package de.gwdg.metadataqa.marc.cli.utils;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class IgnorableFields {
   private List<String> fields;
+  private List<Pattern> maskedFields;
 
   public void parseFields(String raw) {
-    if (StringUtils.isNotBlank(raw))
-      fields = Arrays.asList(raw.split(","));
+    if (StringUtils.isNotBlank(raw)) {
+      fields = new ArrayList<>();
+      maskedFields = new ArrayList<>();
+      List<String> candidates = Arrays.asList(raw.split(","));
+      for (String candidate : candidates) {
+        if (candidate.contains("."))
+          maskedFields.add(Pattern.compile(candidate));
+        else
+          fields.add(candidate);
+      }
+    }
   }
 
   public boolean isEmpty() {
@@ -22,7 +34,20 @@ public class IgnorableFields {
   }
 
   public boolean contains(String tag) {
-    return !isEmpty() && fields.contains(tag);
+    if (isEmpty())
+      return false;
+    if (fields.contains(tag))
+      return true;
+    if (!maskedFields.isEmpty()) {
+      for (Pattern pattern : maskedFields) {
+        if (pattern.matcher(tag).matches()) {
+          fields.add(tag); // caching known fields
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 
   public String toString() {

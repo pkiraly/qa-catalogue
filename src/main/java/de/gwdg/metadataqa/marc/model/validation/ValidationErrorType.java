40be8e89752b977fd8f588bcb476255d94f399c5
Péter Király <pkiraly@gwdg.de>
2022-11-06 22:20:31 +0100

    issue #187: add parameter to exclude issue types

--- src/main/java/de/gwdg/metadataqa/marc/model/validation/ValidationErrorType.java
+++ src/main/java/de/gwdg/metadataqa/marc/model/validation/ValidationErrorType.java
@@ -4,35 +4,35 @@ public enum ValidationErrorType {
 
   // record
   RECORD_UNDETECTABLE_TYPE(1, "undetectableType", "undetectable type", ValidationErrorCategory.RECORD),
-  RECORD_INVALID_LINKAGE(2, "invalidLinkage", "invalid linkage",  ValidationErrorCategory.RECORD),
-  RECORD_AMBIGUOUS_LINKAGE(3, "ambiguousLinkage", "ambiguous linkage",  ValidationErrorCategory.RECORD),
+  RECORD_INVALID_LINKAGE(2, "invalidLinkage", "invalid linkage", ValidationErrorCategory.RECORD),
+  RECORD_AMBIGUOUS_LINKAGE(3, "ambiguousLinkage", "ambiguous linkage", ValidationErrorCategory.RECORD),
 
   // control subfield
   CONTROL_POSITION_OBSOLETE_CODE(4, "obsoleteControlPosition", "obsolete code", ValidationErrorCategory.CONTROLFIELD),
-  CONTROL_POSITION_INVALID_CODE(5, "controlValueContainsInvalidCode", "invalid code",  ValidationErrorCategory.CONTROLFIELD),
-  CONTROL_POSITION_INVALID_VALUE(6, "hasInvalidValue", "invalid value",  ValidationErrorCategory.CONTROLFIELD),
+  CONTROL_POSITION_INVALID_CODE(5, "controlValueContainsInvalidCode", "invalid code", ValidationErrorCategory.CONTROLFIELD),
+  CONTROL_POSITION_INVALID_VALUE(6, "hasInvalidValue", "invalid value", ValidationErrorCategory.CONTROLFIELD),
 
   // field
-  FIELD_MISSING_REFERENCE_SUBFIELD(7, "missingSubfield", "missing reference subfield (880$6)",  ValidationErrorCategory.DATAFIELD),
-  FIELD_NONREPEATABLE(8, "nonrepeatableField", "repetition of non-repeatable field",  ValidationErrorCategory.DATAFIELD),
-  FIELD_UNDEFINED(9, "undefinedField", "undefined field",  ValidationErrorCategory.DATAFIELD),
+  FIELD_MISSING_REFERENCE_SUBFIELD(7, "missingSubfield", "missing reference subfield (880$6)", ValidationErrorCategory.DATAFIELD),
+  FIELD_NONREPEATABLE(8, "nonrepeatableField", "repetition of non-repeatable field", ValidationErrorCategory.DATAFIELD),
+  FIELD_UNDEFINED(9, "undefinedField", "undefined field", ValidationErrorCategory.DATAFIELD),
 
   // indicator
-  INDICATOR_OBSOLETE(10, "obsoleteIndicator", "obsolete value",  ValidationErrorCategory.INDICATOR),
-  INDICATOR_NON_EMPTY(11, "nonEmptyIndicator", "non-empty indicator",  ValidationErrorCategory.INDICATOR),
-  INDICATOR_INVALID_VALUE(12, "hasInvalidValue", "invalid value",  ValidationErrorCategory.INDICATOR),
+  INDICATOR_OBSOLETE(10, "obsoleteIndicator", "obsolete value", ValidationErrorCategory.INDICATOR),
+  INDICATOR_NON_EMPTY(11, "nonEmptyIndicator", "non-empty indicator", ValidationErrorCategory.INDICATOR),
+  INDICATOR_INVALID_VALUE(12, "hasInvalidValue", "invalid value", ValidationErrorCategory.INDICATOR),
 
   // subfield
   SUBFIELD_UNDEFINED(13, "undefinedSubfield", "undefined subfield", ValidationErrorCategory.SUBFIELD),
-  SUBFIELD_INVALID_LENGTH(14, "invalidLength", "invalid length",  ValidationErrorCategory.SUBFIELD),
-  SUBFIELD_INVALID_CLASSIFICATION_REFERENCE(15, "invalidReference", "invalid classification reference",  ValidationErrorCategory.SUBFIELD),
-  SUBFIELD_PATTERN_MISMATCH(16, "patternMismatch", "content does not match any patterns",  ValidationErrorCategory.SUBFIELD),
-  SUBFIELD_NONREPEATABLE(17, "nonrepeatableSubfield", "repetition of non-repeatable subfield",  ValidationErrorCategory.SUBFIELD),
-  SUBFIELD_ISBN(18, "invalidISBN", "invalid ISBN",  ValidationErrorCategory.SUBFIELD),
-  SUBFIELD_ISSN(19, "invalidISSN", "invalid ISSN",  ValidationErrorCategory.SUBFIELD),
-  SUBFIELD_UNPARSABLE_CONTENT(20, "unparsableContent", "content is not well-formatted",  ValidationErrorCategory.SUBFIELD),
-  SUBFIELD_NULL_CODE(21, "nullCode", "null subfield code",  ValidationErrorCategory.SUBFIELD),
-  SUBFIELD_INVALID_VALUE(22, "hasInvalidValue", "invalid value",  ValidationErrorCategory.SUBFIELD),
+  SUBFIELD_INVALID_LENGTH(14, "invalidLength", "invalid length", ValidationErrorCategory.SUBFIELD),
+  SUBFIELD_INVALID_CLASSIFICATION_REFERENCE(15, "invalidReference", "invalid classification reference", ValidationErrorCategory.SUBFIELD),
+  SUBFIELD_PATTERN_MISMATCH(16, "patternMismatch", "content does not match any patterns", ValidationErrorCategory.SUBFIELD),
+  SUBFIELD_NONREPEATABLE(17, "nonrepeatableSubfield", "repetition of non-repeatable subfield", ValidationErrorCategory.SUBFIELD),
+  SUBFIELD_ISBN(18, "invalidISBN", "invalid ISBN", ValidationErrorCategory.SUBFIELD),
+  SUBFIELD_ISSN(19, "invalidISSN", "invalid ISSN", ValidationErrorCategory.SUBFIELD),
+  SUBFIELD_UNPARSABLE_CONTENT(20, "unparsableContent", "content is not well-formatted", ValidationErrorCategory.SUBFIELD),
+  SUBFIELD_NULL_CODE(21, "nullCode", "null subfield code", ValidationErrorCategory.SUBFIELD),
+  SUBFIELD_INVALID_VALUE(22, "hasInvalidValue", "invalid value", ValidationErrorCategory.SUBFIELD),
   ;
 
   private final int id;

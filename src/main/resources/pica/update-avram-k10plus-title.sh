b4c1938069d4e665c8cc357d66993c767f2226b6
Jakob Voss <voss@gbv.de>
2022-11-22 09:29:59 +0100

    K10plus avram schema: remove occurence 00

--- src/main/resources/pica/update-avram-k10plus-title.sh
+++ src/main/resources/pica/update-avram-k10plus-title.sh
@@ -4,5 +4,7 @@ command -v jq >/dev/null 2>&1 || { echo >&2 "Missing command jq"; exit 1; }
 
 # See <https://github.com/pkiraly/metadata-qa-marc/issues/193> why repeatable is changed
 curl "https://format.k10plus.de/avram.pl?profile=k10plus-title" \
-    | jq -S '(.fields[]|select(.subfields.U and .subfields.T)).repeatable=true' > avram-k10plus-title.json
+    | jq -S '(.fields[]|select(.subfields.U and .subfields.T)).repeatable=true' \
+    | jq -S '(.fields[]|select(.occurrence=="00")).occurrence=null' \
+    > avram-k10plus-title.json
 

ee1d4919430cb6dd9f4e3d80d5d8bea5e26b5588
Jakob Voss <voss@gbv.de>
2022-11-22 09:12:12 +0100

    Update avram-k10plus-title.json (solves #193)

--- src/main/resources/pica/update-avram-k10plus-title.sh
+++ src/main/resources/pica/update-avram-k10plus-title.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-wget -O avram-k10plus-title.json "https://format.k10plus.de/avram.pl?profile=k10plus-title"
+command -v jq >/dev/null 2>&1 || { echo >&2 "Missing command jq"; exit 1; }
+
+# See <https://github.com/pkiraly/metadata-qa-marc/issues/193> why repeatable is changed
+curl "https://format.k10plus.de/avram.pl?profile=k10plus-title" \
+    | jq -S '(.fields[]|select(.subfields.U and .subfields.T)).repeatable=true' > avram-k10plus-title.json
+

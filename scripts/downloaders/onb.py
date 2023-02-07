45f400e448599f4e605d24df82527eb52f9afb02
Péter Király <pkiraly@gwdg.de>
2023-02-07 20:07:37 +0100

    Update ONB downloader

--- scripts/downloaders/onb.py
+++ scripts/downloaders/onb.py
@@ -3,12 +3,11 @@ import sys
 from lxml import etree
 from sickle import Sickle
 
-print(len(sys.argv))
 if len(sys.argv) == 1:
     print('Please give a directory name')
     exit()
 dir = sys.argv[1]
-print(dir)
+print('saving to %s' % (dir))
 
 namespaces = {
     'oai': 'http://www.openarchives.org/OAI/2.0/',
@@ -22,6 +21,7 @@ footer = '</records>'
 
 output = []
 i = 0
+record_count = 0
 it = sickle.ListRecords(metadataPrefix='marc21', set='FULLMARC')
 for record in it:
     tree = etree.ElementTree(record.xml)
@@ -31,12 +31,20 @@ for record in it:
     for rec in recs:
         core = etree.tostring(rec, encoding='utf8', method='xml').decode("utf-8")
         output.append(core.replace("<?xml version='1.0' encoding='utf8'?>\n", ''))
+        record_count += 1
     if len(output) >= 1000:
-        file_name = "%s/%06d.xml" % (dir, i)
-        print(file_name)
-        with io.open(file_name, 'w', encoding='utf8') as f:
-            f.write(header)
-            f.write("\n".join(output) + "\n")
-            f.write(footer)
+        write_output(output, i, dir)
         output = []
         i = i + 1
+
+write_output(output, i, dir)
+print('savied %d records to %d files' % (record_count, i))
+
+def write_output(output, i, dir):
+    """Writes MARC records to file""" 
+    file_name = "%s/%06d.xml" % (dir, i)
+    print(file_name)
+    with io.open(file_name, 'w', encoding='utf8') as f:
+        f.write(header)
+        f.write("\n".join(output) + "\n")
+        f.write(footer)

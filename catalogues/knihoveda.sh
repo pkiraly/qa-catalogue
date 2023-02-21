1333a65c46dc775894ed99b14560c9f13d64db73
Péter Király <pkiraly@gwdg.de>
2023-02-21 22:35:09 +0100

    Improve KB downloader

---
+++ catalogues/knihoveda.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+. ./setdir.sh
+NAME=knihoveda
+MARC_DIR=${BASE_INPUT_DIR}/knihoveda/
+TYPE_PARAMS="--marcxml --emptyLargeCollectors"
+MASK=K?.xml
+
+. ./common-script
+
+if [[ "$1" != "help" ]]; then
+  echo "DONE"
+fi
+
+exit 0
---
+++ scripts/downloaders/kb-identifiers.py
@@ -0,0 +1,68 @@
+import io
+import os
+import sys
+from lxml import etree
+from sickle import Sickle
+
+if len(sys.argv) == 1:
+    print('Please give a directory name')
+    exit()
+directory = sys.argv[1]
+print('saving to %s' % (directory))
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+namespaces = {
+    'oai': 'http://www.openarchives.org/OAI/2.0/',
+    'marc21': 'http://www.loc.gov/MARC21/slim'
+}
+
+def write_output(output, i, directory):
+    """Writes MARC records to file""" 
+    file_name = "%s/%06d.xml" % (directory, i)
+    print(file_name)
+    with io.open(file_name, 'w', encoding='utf8') as f:
+        # f.write(header)
+        f.write("\n".join(output) + "\n")
+        # f.write(footer)
+
+#                http://services.kb.nl/mdo/oai?verb=ListIdentifiers&set=GGC&metadataPrefix=mdoall
+sickle = Sickle('http://services.kb.nl/mdo/oai', max_retries=4)
+
+header = '<?xml version="1.0" encoding="utf8"?>' + "\n" + '<records>' + "\n" 
+footer = '</records>'
+
+output = []
+i = 0
+it = sickle.ListIdentifiers(metadataPrefix='mdoall', set='GGC', ignore_deleted=True) # ignore_deleted=True, picaplus
+for record in it:
+    # print(it.resumption_token)
+    # print(record.raw)
+    tree = etree.ElementTree(record.xml)
+    id = tree.xpath('oai:header/oai:identifier', namespaces=namespaces)
+    print(id)
+    # token = tree.xpath('/ListRecords/resumptionToken[0]', namespaces=namespaces)
+    # token = tree.xpath('/ListRecords/resumptionToken[0]', namespaces=namespaces)
+
+    output.append(record.raw)
+
+    #  core = etree.tostring(record, encoding='utf8', method='xml').decode("utf-8")
+    #  output.append(core.replace("<?xml version='1.0' encoding='utf8'?>\n", ''))
+    # output.append(etree.tostring(record, encoding='utf8', method='xml').decode("utf-8"))
+    # tree = etree.ElementTree(record.xml)
+    # token = tree.xpath('/ListRecords/resumptionToken[0]', namespaces=namespaces)
+    # print(token)
+    # print(etree.tostring(token, encoding='utf8', method='xml').decode("utf-8"))
+
+    # recs = tree.xpath('/oai:record[*]/oai:metadata/marc21:record', namespaces=namespaces)
+    # for rec in recs:
+    #     core = etree.tostring(rec, encoding='utf8', method='xml').decode("utf-8")
+    #     output.append(core.replace("<?xml version='1.0' encoding='utf8'?>\n", ''))
+    if len(output) >= 100:
+        write_output(output, i, directory)
+        output = []
+        i = i + 1
+
+write_output(output, i, directory)
+print('savied %d records to %d files' % (record_count, i))
+

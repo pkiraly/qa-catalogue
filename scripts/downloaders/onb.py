224f01d00edb61cc07d1641590b26d7baf76bf8c
Péter Király <pkiraly@gwdg.de>
2023-02-24 16:50:10 +0000

    Improve downloaders

--- scripts/downloaders/onb.py
+++ scripts/downloaders/onb.py
@@ -12,11 +12,20 @@ print('saving to %s' % (dir))
 namespaces = {
     'oai': 'http://www.openarchives.org/OAI/2.0/',
     'marc21': 'http://www.loc.gov/MARC21/slim'
-} 
+}
+
+def write_output(output, i, dir):
+    """Writes MARC records to file"""
+    file_name = "%s/%06d.xml" % (dir, i)
+    print(file_name)
+    with io.open(file_name, 'w', encoding='utf8') as f:
+        f.write(header)
+        f.write("\n".join(output) + "\n")
+        f.write(footer)
 
 sickle = Sickle('https://eu02.alma.exlibrisgroup.com/view/oai/43ACC_ONB/request', max_retries=4)
 
-header = '<?xml version="1.0" encoding="utf8"?>' + "\n" + '<records>' + "\n" 
+header = '<?xml version="1.0" encoding="utf8"?>' + "\n" + '<records>' + "\n"
 footer = '</records>'
 
 output = []
@@ -26,25 +35,17 @@ it = sickle.ListRecords(metadataPrefix='marc21', set='FULLMARC')
 for record in it:
     tree = etree.ElementTree(record.xml)
     token = tree.xpath('/resumptionToken', namespaces=namespaces)
-    
+
     recs = tree.xpath('/oai:record[*]/oai:metadata/marc21:record', namespaces=namespaces)
     for rec in recs:
         core = etree.tostring(rec, encoding='utf8', method='xml').decode("utf-8")
         output.append(core.replace("<?xml version='1.0' encoding='utf8'?>\n", ''))
         record_count += 1
-    if len(output) >= 1000:
+    if len(output) >= 10000:
         write_output(output, i, dir)
         output = []
         i = i + 1
 
 write_output(output, i, dir)
-print('savied %d records to %d files' % (record_count, i))
+print('saved %d records to %d files' % (record_count, i))
 
-def write_output(output, i, dir):
-    """Writes MARC records to file""" 
-    file_name = "%s/%06d.xml" % (dir, i)
-    print(file_name)
-    with io.open(file_name, 'w', encoding='utf8') as f:
-        f.write(header)
-        f.write("\n".join(output) + "\n")
-        f.write(footer)

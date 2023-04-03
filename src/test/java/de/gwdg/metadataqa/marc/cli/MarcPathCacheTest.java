c2d01289ddeebacb0094f449b7e3b85609906f4c
Jean Michel Nzi Mba <121430467+JMichel92@users.noreply.github.com>
2023-04-03 14:52:00 +0200

    DataField issues in MarcPathCacheTest fixed

--- src/test/java/de/gwdg/metadataqa/marc/cli/MarcPathCacheTest.java
+++ src/test/java/de/gwdg/metadataqa/marc/cli/MarcPathCacheTest.java
@@ -1,16 +1,13 @@
 package de.gwdg.metadataqa.marc.cli;
 
 import de.gwdg.metadataqa.api.model.XmlFieldInstance;
-import de.gwdg.metadataqa.marc.analysis.AuthorityCategory;
-import de.gwdg.metadataqa.marc.analysis.ShelfReadyFieldsBooks;
-import de.gwdg.metadataqa.marc.analysis.ThompsonTraillFields;
 import de.gwdg.metadataqa.marc.dao.DataField;
 import de.gwdg.metadataqa.marc.dao.record.BibliographicRecord;
-import de.gwdg.metadataqa.marc.definition.MarcVersion;
+import de.gwdg.metadataqa.marc.dao.record.Marc21Record;
+import de.gwdg.metadataqa.marc.definition.tags.tags20x.Tag245;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.List;
-import java.util.Map;
 
 public class MarcPathCacheTest {
 
@@ -21,101 +18,26 @@ public class MarcPathCacheTest {
 
     @Test
     public void testGet() {
-        BibliographicRecord record = new BibliographicRecord() {
-            @Override
-            public List<DataField> getAuthorityFields() {
-                return null;
-            }
+        BibliographicRecord marcRecord = new Marc21Record("u2407796");
 
-            @Override
-            public Map<DataField, AuthorityCategory> getAuthorityFieldsMap() {
-                return null;
-            }
+        marcRecord.addDataField(new DataField(Tag245.getInstance(), "0", "0",
+                "6", "880-01",
+                "a", "iPhone the Bible wan jia sheng jing."));
 
-            @Override
-            public boolean isAuthorityTag(String tag) {
-                return false;
-            }
-
-            @Override
-            public boolean isSkippableAuthoritySubfield(String tag, String code) {
-                return false;
-            }
-
-            @Override
-            public boolean isSubjectTag(String tag) {
-                return false;
-            }
-
-            @Override
-            public boolean isSkippableSubjectSubfield(String tag, String code) {
-                return false;
-            }
-
-            @Override
-            public Map<ShelfReadyFieldsBooks, Map<String, List<String>>> getShelfReadyMap() {
-                return null;
-            }
-
-            @Override
-            public Map<ThompsonTraillFields, List<String>> getThompsonTraillTagsMap() {
-                return null;
-            }
-        };
-        record.addDataField(new DataField("245", "0", "0", "Title" ,MarcVersion.MARC21));
-        MarcPathCache pathCache = new MarcPathCache(record);
+        MarcPathCache pathCache = new MarcPathCache(marcRecord);
         List<XmlFieldInstance> results = pathCache.get("245$a");
-        //List<XmlFieldInstance> results = pathCache.get("245$atitle");
-        assertEquals(0, results.size());
-        //assertEquals("Title", results.get(0).getValue());
-        assertTrue(results.isEmpty());
+        assertEquals(1, results.size());
+        assertEquals("iPhone the Bible wan jia sheng jing.", results.get(0).getValue());
     }
 
     @Test
     public void testGetNonexistentField() {
-        BibliographicRecord record = new BibliographicRecord() {
-            @Override
-            public List<DataField> getAuthorityFields() {
-                return null;
-            }
 
-            @Override
-            public Map<DataField, AuthorityCategory> getAuthorityFieldsMap() {
-                return null;
-            }
-
-            @Override
-            public boolean isAuthorityTag(String tag) {
-                return false;
-            }
-
-            @Override
-            public boolean isSkippableAuthoritySubfield(String tag, String code) {
-                return false;
-            }
-
-            @Override
-            public boolean isSubjectTag(String tag) {
-                return false;
-            }
-
-            @Override
-            public boolean isSkippableSubjectSubfield(String tag, String code) {
-                return false;
-            }
-
-            @Override
-            public Map<ShelfReadyFieldsBooks, Map<String, List<String>>> getShelfReadyMap() {
-                return null;
-            }
-
-            @Override
-            public Map<ThompsonTraillFields, List<String>> getThompsonTraillTagsMap() {
-                return null;
-            }
-        };
-        record.addDataField(new DataField("245", "0", "0","Title" ,MarcVersion.MARC21));
-        MarcPathCache pathCache = new MarcPathCache(record);
+        BibliographicRecord marcRecord = new Marc21Record("u2407796");
+        marcRecord.addDataField(new DataField(Tag245.getInstance(), "0", "0",
+                "6", "880-01",
+                "a", "iPhone the Bible wan jia sheng jing."));
+        MarcPathCache pathCache = new MarcPathCache(marcRecord);
         List<XmlFieldInstance> results = pathCache.get("100");
         assertTrue(results.isEmpty());
     }

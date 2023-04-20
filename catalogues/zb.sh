5f3c5597c8f61b0043435147aca9bf782054bc14
Péter Király <pkiraly@gwdg.de>
2023-04-20 15:42:29 +0200

    Fixing 'all' types

--- catalogues/zb.sh
+++ catalogues/zb.sh
@@ -7,7 +7,6 @@ NAME=zb
 MARC_DIR=${BASE_INPUT_DIR}/zb/
 TYPE_PARAMS="--marcxml --fixAlma --emptyLargeCollectors"
 # MASK=BIB*.xml
-# MASK=lines.xml
-MASK=datadump-zb3.xml
+MASK=lines.xml
 
 . ./common-script

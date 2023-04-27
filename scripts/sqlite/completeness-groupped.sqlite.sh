13ac5a29ac940005dee2263fe9be457c1692dd05
Péter Király <pkiraly@gwdg.de>
2023-04-27 12:13:35 +0200

    Group results in completeness: import into DB #199

--- scripts/sqlite/completeness-groupped.sqlite.sh
+++ scripts/sqlite/completeness-groupped.sqlite.sh
@@ -1,3 +1,19 @@
+#!/usr/bin/env bash
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Strores completeness-groupped-marc-elements.csv into SQLite
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+log() {
+  timestamp=`date +"%F %T"`
+  echo -en "\033[0D\033[1;37m$timestamp>\033[0m "
+  echo "$1"
+}
+
+OUTPUT_DIR=$1
+
+log "create table groupped_marc_elements"
 sqlite3 ${OUTPUT_DIR}/qa_catalogue.sqlite << EOF
 CREATE TABLE IF NOT EXISTS "groupped_marc_elements" (
   "groupId"             INTEGER,
@@ -18,12 +34,19 @@ CREATE TABLE IF NOT EXISTS "groupped_marc_elements" (
 CREATE INDEX IF NOT EXISTS "groupId" ON "groupped_marc_elements" ("groupId");
 EOF
 
+log "clean groupped_marc_elements"
+sqlite3 ${OUTPUT_DIR}/qa_catalogue.sqlite << EOF
+DELETE FROM groupped_marc_elements;
+EOF
+
+log "create headless CSV"
 tail -n +2 ${OUTPUT_DIR}/completeness-groupped-marc-elements.csv > ${OUTPUT_DIR}/completeness-groupped-marc-elements-noheader.csv
 
-echo "import marc elements"
+log "import marc elements"
 sqlite3 ${OUTPUT_DIR}/qa_catalogue.sqlite << EOF
 .mode csv
 .import ${OUTPUT_DIR}/completeness-groupped-marc-elements-noheader.csv groupped_marc_elements
 EOF
 
+log "drop headless CSV"
 rm ${OUTPUT_DIR}/completeness-groupped-marc-elements-noheader.csv

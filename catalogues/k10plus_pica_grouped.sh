f9763c8cdb64888100b9dca965bd6e886ae09936
Jakob Voss <voss@gbv.de>
2023-06-23 10:46:51 +0200

    Default type params when SCHEMA=PICA (close #287)

--- catalogues/k10plus_pica_grouped.sh
+++ catalogues/k10plus_pica_grouped.sh
@@ -3,7 +3,7 @@
 . ./setdir.sh
 
 SCHEMA=PICA
-TYPE_PARAMS="--schemaType PICA --marcFormat PICA_NORMALIZED --emptyLargeCollectors"
+TYPE_PARAMS="--emptyLargeCollectors"
 TYPE_PARAMS="$TYPE_PARAMS --groupBy 001@\$0"
 TYPE_PARAMS="$TYPE_PARAMS --groupListFile src/main/resources/k10plus-libraries-by-unique-iln.txt"
 TYPE_PARAMS="$TYPE_PARAMS --ignorableFields 001@,001E,001L,001U,001U,001X,001X,002V,003C,003G,003Z,008G,017N,020F,027D,031B,037I,039V,042@,046G,046T,101@,101E,101U,102D,201E,201U,202D,1...,2..."

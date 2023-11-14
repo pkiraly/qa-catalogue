d9a842cd2d555cb76957785d7ddd8dab739a26c4
Péter Király <pkiraly@gwdg.de>
2023-11-14 22:53:48 +0100

    Create class for Yale

--- catalogues/yale.sh
+++ catalogues/yale.sh
@@ -5,6 +5,6 @@
 NAME=yale
 MARC_DIR=${BASE_INPUT_DIR}/yale/2023-11-05
 TYPE_PARAMS="--emptyLargeCollectors --indexWithTokenizedField --indexFieldCounts"
-MASK=bib_*.mrc
+MASK=bib_*.mrc.gz
 
 . ./common-script

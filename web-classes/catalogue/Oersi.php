37c1b39d9b6ab94d75c36645d0018676d1d21cbf
Phu Tu <phu.tu@hbz-nrw.de>
2024-05-03 12:28:32 +0200

    Change marcVersion in web config for oersi

--- web-classes/catalogue/Oersi.php
+++ web-classes/catalogue/Oersi.php
@@ -4,6 +4,6 @@ class Oersi extends Catalogue {
   protected $name = 'oersi';
   protected $label = 'OERSI MARC Test';
   protected $url = 'https://example.com/';
-  protected $marcVersion = 'DNB';
+  protected $marcVersion = 'ZB';
   protected $linkTemplate = 'http://example.com/{id}';
 }

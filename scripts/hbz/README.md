cc21f29a42551c8e6a3c0d13b63efa2be8939760
Phu Tu <phu.tu@hbz-nrw.de>
2024-05-03 12:13:52 +0200

    Add helper script for running analyses
    via cron or manually

---
+++ scripts/hbz/README.md
@@ -0,0 +1,13 @@
+# Usage
+
+## via cron
+
+see /etc/crontab
+
+## manually
+
+run script as user `qa-catalogue`
+
+### oersi
+
+`nohup ./run-analyses-if-new-input-files.sh oersi > nohup_oersi.out 2>&1 &`
---
+++ scripts/hbz/run-analyses-if-new-input-files.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# run this script with name of your catalogue as argument
+# if no argument given defaults to test
+
+APPDIR="/opt/qa-catalogue"
+CATALOGUE=${1:-test}
+DATADIR="${APPDIR}/input/${CATALOGUE}/marc"
+CHECKFILE="${DATADIR}/check_file.txt"
+RUNNINGFILE="${APPDIR}/running_${CATALOGUE}_analyses.txt"
+
+find ${DATADIR} -newer "$CHECKFILE" >/tmp/newfiles$$
+
+if [[ -s "/tmp/newfiles$$" ]] ; then
+  echo "files changed: "
+  cat "/tmp/newfiles$$"
+  echo "$(date) Now start qa catalogue analyses"
+  if [[ -f "$RUNNINGFILE" ]]; then
+    echo "$(date) Oops, there are analyses still running! Try again later."
+  else
+    touch "$RUNNINGFILE"
+    cd $APPDIR
+    docker exec -i metadata-qa-marc ./catalogues/${CATALOGUE}.sh all
+    echo "$(date) Finished analyses for ${CATALOGUE}!"
+    rm "$RUNNINGFILE"
+    touch "$CHECKFILE"
+    rm "/tmp/newfiles$$"
+  fi
+else
+  echo "No files changed"
+  touch "$CHECKFILE"
+  rm "/tmp/newfiles$$"
+fi

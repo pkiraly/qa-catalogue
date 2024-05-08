3e0b8b1d9f3a3a4dc775d3e982231ec03e084498
Phu Tu <phu.tu@hbz-nrw.de>
2024-05-08 15:37:01 +0200

    Change marcVersion from DNB to MARC21

--- catalogues/oersi.sh
+++ catalogues/oersi.sh
@@ -6,13 +6,12 @@ NAME=oersi
 TYPE=xml
 if [[ $TYPE == "marc" ]]; then
   echo "marc"
-  TYPE_PARAMS="--marcVersion DNB"
+  TYPE_PARAMS="--marcVersion MARC21"
   MARC_DIR=${BASE_INPUT_DIR}/oersi/marc
   MASK=*.mrc
 elif [[ $TYPE == "xml" ]]; then
   echo "xml"
-  # TYPE_PARAMS="--marcVersion DNB --marcxml"
-  TYPE_PARAMS="--marcVersion ZB --marcxml --fixAlma --emptyLargeCollectors"
+  TYPE_PARAMS="--marcVersion MARC21 --marcxml"
   MARC_DIR=${BASE_INPUT_DIR}/oersi/marc
   MASK=*.gz
 else

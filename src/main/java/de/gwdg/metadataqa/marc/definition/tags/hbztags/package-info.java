704370ad52ebc7d4665dc43c2371099dac326b01
Tobias Bülte <tobias.buelte@hbz-nrw.de>
2024-08-19 11:36:52 +0200

    Adjust package info for hbz
    Adjustment suggested by @maipet

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/hbztags/package-info.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/hbztags/package-info.java
@@ -3,14 +3,9 @@
  *
  * The source of the classes is this document:
  *
- * Feldbeschreibung der Titeldaten der Deutschen Nationalbibliothek und der
- * Zeitschriftendatenbank im Format MARC 21
- *
- * Version 2.4
- * Stand: 16. Oktober 2017
- *
- * Available at http://www.dnb.de/EN/Standardisierung/Formate/MARC21/marc21_node.html
+ * https://service-wiki.hbz-nrw.de/display/VDBE/Feldinformationen+NZ+Alma
+ * and https://service-wiki.hbz-nrw.de/pages/viewpage.action?pageId=949911658
  */
-package de.gwdg.metadataqa.marc.definition.tags.dnbtags;
+package de.gwdg.metadataqa.marc.definition.tags.hbztags;
 
 //rest of the file is empty

10a4c8462d433ff075345a3fa81aabef27936698
Tobias Bülte <tobias.buelte@hbz-nrw.de>
2024-08-19 10:44:01 +0200

    Add hbz element 964

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/hbztags/Tag964.java
@@ -0,0 +1,42 @@
+package de.gwdg.metadataqa.marc.definition.tags.hbztags;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.structure.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.structure.Indicator;
+
+/**
+ * In Aleph benötigte Informationen (Wickelfeld)
+ */
+public class Tag964 extends DataFieldDefinition {
+
+  private static Tag964 uniqueInstance;
+
+  private Tag964() {
+    initialize();
+    postCreation();
+  }
+
+  public static Tag964 getInstance() {
+    if (uniqueInstance == null)
+      uniqueInstance = new Tag964();
+    return uniqueInstance;
+  }
+
+  private void initialize() {
+    tag = "964";
+    label = "In Aleph benötigte Informationen (Wickelfeld)";
+    mqTag = "InAlephbenötigteInformationenWickelfeld";
+    cardinality = Cardinality.Nonrepeatable;
+    // descriptionUrl = "https://service-wiki.hbz-nrw.de/pages/viewpage.action?pageId=724304028";
+
+    ind1 = new Indicator();
+    ind2 = new Indicator();
+
+    setSubfieldsWithCardinality(
+      "A",	"A", "R",
+      "F",	"F", "R",
+      "V",	"V", "R"
+    );
+
+  }
+}

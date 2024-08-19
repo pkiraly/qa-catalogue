66aaaf8f20015e497d33d4a5ee3cbbce87672e7d
Tobias Bülte <tobias.buelte@hbz-nrw.de>
2024-08-19 10:40:13 +0200

    Add hbz element 962

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/hbztags/Tag962.java
@@ -0,0 +1,40 @@
+package de.gwdg.metadataqa.marc.definition.tags.hbztags;
+
+import de.gwdg.metadataqa.marc.definition.Cardinality;
+import de.gwdg.metadataqa.marc.definition.structure.DataFieldDefinition;
+import de.gwdg.metadataqa.marc.definition.structure.Indicator;
+
+/**
+ * Selektionskennzeichen EBooks, Digitalisate
+ */
+public class Tag962 extends DataFieldDefinition {
+
+  private static Tag962 uniqueInstance;
+
+  private Tag962() {
+    initialize();
+    postCreation();
+  }
+
+  public static Tag962 getInstance() {
+    if (uniqueInstance == null)
+      uniqueInstance = new Tag962();
+    return uniqueInstance;
+  }
+
+  private void initialize() {
+    tag = "962";
+    label = "Selektionskennzeichen EBooks, Digitalisate";
+    mqTag = "SelektionskennzeichenEBooksDigitalisate";
+    cardinality = Cardinality.Nonrepeatable;
+    // descriptionUrl = "https://service-wiki.hbz-nrw.de/display/VDBE/962+-+Selektionskennzeichen+EBooks%2C+Digitalisate";
+
+    ind1 = new Indicator();
+    ind2 = new Indicator();
+
+    setSubfieldsWithCardinality(
+      "e",	"Selektionskennzeichen EBooks, Digitalisate", "R"
+    );
+
+  }
+}

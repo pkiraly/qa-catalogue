06d732b5cc0935ad7bfa5847e7f0c086bdfe6fe6
Phu Tu <phu.tu@hbz-nrw.de>
2024-08-20 15:54:37 +0200

    Correct number of local (sub)fields reported in build log

--- src/test/java/de/gwdg/metadataqa/marc/utils/DataElementsStaticticsTest.java
+++ src/test/java/de/gwdg/metadataqa/marc/utils/DataElementsStaticticsTest.java
@@ -15,9 +15,9 @@ public class DataElementsStaticticsTest {
     assertEquals( 229, statistics.get(DataElementType.coreFields));
     assertEquals( 184, statistics.get(DataElementType.coreIndicators));
     assertEquals(2667, statistics.get(DataElementType.coreSubfields));
-    assertEquals( 228, statistics.get(DataElementType.localFields));
+    assertEquals( 262, statistics.get(DataElementType.localFields));
     assertEquals(  30, statistics.get(DataElementType.localIndicators));
-    assertEquals(1804, statistics.get(DataElementType.localSubfields));
-    assertEquals(5360, statistics.total());
+    assertEquals(2005, statistics.get(DataElementType.localSubfields));
+    assertEquals(5595, statistics.total());
   }
 }

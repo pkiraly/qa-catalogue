d00a10a3cf291475b05fffeb9ca56440eed7d39b
Phu Tu <phu.tu@hbz-nrw.de>
2024-08-28 09:20:19 +0200

    Add hbz-update catalogue

---
+++ catalogues/hbz-update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+. ./setdir.sh
+
+NAME=hbz-update
+TYPE=xml
+if [[ $TYPE == "marc" ]]; then
+  echo "marc"
+  TYPE_PARAMS="--marcVersion HBZ --fixAlma"
+  MARC_DIR=${BASE_INPUT_DIR}/${NAME}/marc
+  MASK=*.mrc
+elif [[ $TYPE == "xml" ]]; then
+  echo "xml"
+  TYPE_PARAMS="--marcVersion DNB,HBZ --marcxml --fixAlma"
+  MARC_DIR=${BASE_INPUT_DIR}/${NAME}/marc
+  MASK=*.gz
+else
+  echo "else: ${TYPE}"
+fi
+
+. ./common-script
---
+++ web-classes/catalogue/Hbz_update.php
@@ -0,0 +1,9 @@
+<?php
+
+class Hbz_update extends Catalogue {
+  protected $name = 'hbz_update';
+  protected $label = 'hbz Verbunddaten Update vom 21.08.2024';
+  protected $url = 'https://example.com/';
+  protected $marcVersion = 'HBZ';
+  protected $linkTemplate = 'http://example.com/{id}';
+}

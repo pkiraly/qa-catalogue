324d1f7958c121e8d234f814471c8873020c7c28
Tobias Bülte <tobias.buelte@hbz-nrw.de>
2024-08-29 10:47:04 +0200

    Update tests

--- src/test/java/de/gwdg/metadataqa/marc/utils/DataElementsStaticticsTest.java
+++ src/test/java/de/gwdg/metadataqa/marc/utils/DataElementsStaticticsTest.java
@@ -15,7 +15,7 @@ public class DataElementsStaticticsTest {
     assertEquals( 229, statistics.get(DataElementType.coreFields));
     assertEquals( 184, statistics.get(DataElementType.coreIndicators));
     assertEquals(2667, statistics.get(DataElementType.coreSubfields));
-    assertEquals( 265, statistics.get(DataElementType.localFields));
+    assertEquals( 291, statistics.get(DataElementType.localFields));
     assertEquals(  47, statistics.get(DataElementType.localIndicators));
     assertEquals(2204, statistics.get(DataElementType.localSubfields));
     assertEquals(5814, statistics.total());

f367430725da39e405d8649ae66ee77cb4b5a816
Tobias Bülte <tobias.buelte@hbz-nrw.de>
2024-08-29 16:37:38 +0200

    Add indicators for H66

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/hbztags/TagH66.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/hbztags/TagH66.java
@@ -30,8 +30,25 @@ public class TagH66 extends DataFieldDefinition {
     cardinality = Cardinality.Repeatable;
     descriptionUrl = "https://service-wiki.hbz-nrw.de/pages/viewpage.action?pageId=949911658";
 
-    ind1 = new Indicator();
-    ind2 = new Indicator();
+    ind1 = new Indicator("Field encoding level")
+    .setCodes(
+      " ", "No information provided",
+      "3", "Holdings level 3",
+      "4", "Holdings level 4",
+      "5", "Holdings level 4 with piece designation"
+    )
+    .setMqTag("FieldEncodingLevel");
+
+
+    ind2 = new Indicator("Type of notation")
+    .setCodes(
+      "0", "Non-standard",
+      "1", "ANSI/NISO Z39.71 or ISO 10324",
+      "2", "ANSI Z39.42",
+      "7", "Source specified in subfield $2 "
+    )
+
+    .setMqTag("TypeOfNotation");
 
     setSubfieldsWithCardinality(
       "a", "Textual holdings", "NR",

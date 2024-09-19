c778daf48198b1e60d4ce03290c072d1c5c0c4ee
Tobias Bülte <tobias.buelte@hbz-nrw.de>
2024-09-19 13:08:51 +0200

    Fix catalogue scripts

--- catalogues/hbz.sh
+++ catalogues/hbz.sh
@@ -11,7 +11,7 @@ if [[ $TYPE == "marc" ]]; then
   MASK=*.mrc
 elif [[ $TYPE == "xml" ]]; then
   echo "xml"
-  TYPE_PARAMS="--marcVersion HBZ --marcxml --fixAlma --ignorableRecords DEL$a=Y  --ignorableFields 964 940 941 942 944 945 946 947 948 949 950 951 952 955 956 957 958 959 966 967 970 971 972 973 974 975 976 977 978 978 979"
+  TYPE_PARAMS="--marcVersion HBZ --marcxml --fixAlma --ignorableRecords DEL$a=Y  --ignorableFields 964,940,941,942,944,945,946,947,948,949,950,951,952,955,956,957,958,959,966,967,970,971,972,973,974,975,976,977,978,978,979"
   MARC_DIR=${BASE_INPUT_DIR}/${NAME}/marc
   MASK=*.gz
 else

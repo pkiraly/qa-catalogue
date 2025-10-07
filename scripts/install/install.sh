9e5245f1be149083ea0911538034281e30bb14e0
Péter Király <pkiraly@gwdg.de>
2025-10-07 22:32:08 +0200

    One liner docker based installation #649

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -3,15 +3,18 @@
 # download the minimum set of files necessary for run the dockerized QA Catalogue
 #
 
+# create directories
+mkdir docker input output web-config
+
 # download necessary files
 wget --inet4-only --quiet https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker-compose.yml
 wget --inet4-only --quiet https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker-compose-slim.yml
 wget --inet4-only --quiet https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/scripts/install/qa-catalogue.env
-mkdir docker
+wget --inet4-only --quiet \
+  -o docker/configuration.cnf
+  https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/scripts/install/web-configuration.cnf
 cd docker
 wget --inet4-only --quiet https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker/qa-catalogue
 chmod +x qa-catalogue
 cd ..
 
-# create directories
-mkdir input output web-config

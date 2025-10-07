2a916a68c7a39cdd76bab9c38d5f7ae21b8f2b01
Péter Király <pkiraly@gwdg.de>
2025-10-07 21:20:40 +0200

    One liner docker based installation #649

--- scripts/downloaders/install.sh
+++ scripts/downloaders/install.sh
@@ -3,9 +3,9 @@
 # download the minimum set of files necessary for run the dockerized QA Catalogue
 #
 
-curl -fsSL --output docker-compose.yml https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker-compose.yml
+wget -4 https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker-compose.yml
 mkdir docker
 cd docker
-curl -fsSL --output qa-catalogue https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker/qa-catalogue
+wget -4 https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker/qa-catalogue
 chmod +x qa-catalogue
 cd ..

2282bb642d2f0152857ab38b2683ea4bafcebe7e
Péter Király <pkiraly@gwdg.de>
2025-10-08 17:57:02 +0200

    One liner docker based installation #649

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -19,5 +19,17 @@ wget --inet4-only --quiet --output-document=docker/qa-catalogue \
   https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker/qa-catalogue
 wget --inet4-only --quiet --output-document=docker/solr-entrypoint/copy-configsets.sh \
   https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker/solr-entrypoint/copy-configsets.sh
+
+# making it executable
 chmod +x docker/qa-catalogue
+chmod +x service.sh
 sudo chown -R 8983:8983 ./solr-data && sudo chmod -R 777 ./solr-data
+
+echo "The stage is yours! Typical next steps:"
+echo "1) copy bibliographical records to `input` directory, adjust `qa-catalogue.env`, and `web-config/configuration.cnf`"
+echo "2) start services with `./service.sh up`"
+echo "3) run analyses with `docker/qa-catalogue ...`"
+echo "4) check it at http://localhost:90"
+echo "if you do not need it anymore"
+echo "5) shut services down with `./service.sh down`"
+echo "6) remove all files with `./service.sh clear-files`"

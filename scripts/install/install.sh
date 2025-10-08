b87a200df9630daaf3bf0a85adf750a1a28692e4
Péter Király <pkiraly@gwdg.de>
2025-10-08 11:20:11 +0200

    One liner docker based installation #649

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -4,7 +4,7 @@
 #
 
 # create directories
-mkdir docker input output logs web-config solr-data
+mkdir docker input output logs web-config solr-data docker/solr-entrypoint
 
 # download necessary files
 wget --inet4-only --quiet --output-document=docker-compose.yml \
@@ -15,6 +15,8 @@ wget --inet4-only --quiet --output-document=web-config/configuration.cnf \
   https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/scripts/install/web-configuration.cnf
 wget --inet4-only --quiet --output-document=docker/qa-catalogue \
   https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker/qa-catalogue
+wget --inet4-only --quiet --output-document=docker/solr-entrypoint/copy-configsets.sh \
+  https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker/solr-entrypoint/copy-configsets.sh
 chmod +x docker/qa-catalogue
 
 

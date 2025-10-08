b781249f5e1c4224fe9334371e2f70f762b791c3
Péter Király <pkiraly@gwdg.de>
2025-10-08 17:40:26 +0200

    One liner docker based installation #649

--- scripts/install/service.sh
+++ scripts/install/service.sh
@@ -1,31 +1,50 @@
 #!/usr/bin/env bash
 
-up() {
+set -e
+ARGV="$@"
+
+up () {
+  echo "up()"
   docker compose --env-file qa-catalogue.env up -d
 }
 
-down() {
+down () {
   docker compose --env-file qa-catalogue.env down
 }
 
-clear_files() {
+clear_files () {
   docker rmi $(docker image ls --filter reference=*/pkiraly/qa-catalogue* -q)
   docker rmi $(docker images solr:9.6.1 -q)
   sudo rm -rf *
 }
 
+usage () {
+  echo "$0 <command>"
+  echo "up|u - start docker services"
+  echo "down|d - stop docker services"
+  echo "clear-files|c - remove docker images and all the files in the current directory"
+}
+
 if [ $# -eq 1 ]; then
   case $1 in
-    -up|-u)
+    up|u)
         up
-        exit $?
+        exit
     ;;
-    -down|-d)
+    down|d)
         down
-        exit $?
+        exit
     ;;
-    -clear-files|-c)
+    clear-files|c)
         clear_files
+        exit
+    ;;
+    help|h)
+        usage
+        exit
+    ;;
+    *)
+        echo "Unrecognised command: $1"
         exit
     ;;
   esac

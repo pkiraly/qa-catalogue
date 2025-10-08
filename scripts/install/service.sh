a8896c42d05e39740f70f8208e66bf93f108adc6
Péter Király <pkiraly@gwdg.de>
2025-10-08 21:35:06 +0200

    One liner docker based installation #649

--- scripts/install/service.sh
+++ scripts/install/service.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env bash
-
+#
+# Managing docker services and files for QA Catalogue
+#
 set -e
 ARGV="$@"
 
 up () {
-  echo "Starting docker services..."
+  echo "Creating docker containers..."
   docker compose --env-file qa-catalogue.env up -d
 }
 
 down () {
-  echo "Stoping docker services..."
+  echo "Removing docker containers..."
   docker compose --env-file qa-catalogue.env down
+}
+
+start () {
+  echo "Starting docker containers..."
+  docker compose --env-file qa-catalogue.env start solr
+  docker compose --env-file qa-catalogue.env start cli
+  docker compose --env-file qa-catalogue.env start app
+}
+
+stop () {
+  echo "Stoping docker containers..."
+  docker compose --env-file qa-catalogue.env stop cli
+  docker compose --env-file qa-catalogue.env stop app
+  docker compose --env-file qa-catalogue.env stop solr
 }
 
 clear_files () {
@@ -29,10 +45,16 @@ clear_files () {
 }
 
 usage () {
+  echo "Managing docker services and files for QA Catalogue"
+  echo ""
+  echo "usage:"
   echo "$0 <command>"
-  echo "up|u - start docker services"
-  echo "down|d - stop docker services"
-  echo "clear-files|c - remove docker images and all the files in the current directory"
+  echo "commands:"
+  echo "- up|u - create and start docker containers"
+  echo "- down|d - stop and remove docker containers"
+  echo "- start - start docker containers"
+  echo "- stop - stop docker containers"
+  echo "- clear-files|c - remove docker images and all the files in the current directory"
 }
 
 if [ $# -eq 1 ]; then
@@ -43,6 +65,14 @@ if [ $# -eq 1 ]; then
     ;;
     down|d)
         down
+        exit
+    ;;
+    start)
+        start
+        exit
+    ;;
+    stop)
+        stop
         exit
     ;;
     clear-files|c)

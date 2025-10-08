4a4271367d3af75b8e39f9a78934b7450296f79b
Péter Király <pkiraly@gwdg.de>
2025-10-08 11:46:24 +0200

    One liner docker based installation #649

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -18,5 +18,4 @@ wget --inet4-only --quiet --output-document=docker/qa-catalogue \
 wget --inet4-only --quiet --output-document=docker/solr-entrypoint/copy-configsets.sh \
   https://raw.githubusercontent.com/pkiraly/qa-catalogue/main/docker/solr-entrypoint/copy-configsets.sh
 chmod +x docker/qa-catalogue
-
-
+sudo chown -R 8983:8983 ./solr-data && sudo chmod -R 777 ./solr-data

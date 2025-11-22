891e20620c8a0e14138f4d179a90019bf9756b4d
Péter Király <pkiraly@gwdg.de>
2025-11-22 12:52:50 +0100

    Add JavaDoc to quarto based documentation #713

--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/sztetags/package-info.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/sztetags/package-info.java
@@ -1,7 +1,7 @@
 /**
  * This package contains the tags specifics at library of the University of Szeged (Hungary)
  *
- * The source of the classes is this document is an email from László Bernátsky <laszlo.bernatsky@ek.szte.hu>
+ * The source of the classes in the package is an email from László Bernátsky (laszlo.bernatsky (at) ek.szte.hu)
  */
 package de.gwdg.metadataqa.marc.definition.tags.sztetags;
 
--- src/main/java/de/gwdg/metadataqa/marc/definition/tags/uvatags/package-info.java
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/uvatags/package-info.java
@@ -1,7 +1,7 @@
 /**
  * This package contains the tags specifics at library of the University of Amsterdam (The Netherlands)
  *
- * The source of the classes is this document is an Excel file I received from from Hadewijch Dekker <h.e.dekker@uva.nl>
+ * The source of the classes in this package is an Excel file I received from Hadewijch Dekker (h.e.dekker (at) uva.nl)
  */
 package de.gwdg.metadataqa.marc.definition.tags.uvatags;
 

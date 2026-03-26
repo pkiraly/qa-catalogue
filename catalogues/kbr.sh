a57646794823bf0a1e320726fab44829ab4c5cb9
Péter Király <pkiraly@gwdg.de>
2026-03-26 09:53:10 +0100

    Tuning KBR configuration

--- catalogues/kbr.sh
+++ catalogues/kbr.sh
@@ -11,7 +11,7 @@ MARC_DIR=${BASE_INPUT_DIR}/current
 TYPE_PARAMS="--emptyLargeCollectors --marcVersion KBR --marcxml --fixKbr"
 # 911
 TYPE_PARAMS="${TYPE_PARAMS} --ignorableFields 590,591,592,593,594,595,596,659,900,912,916,940,941,942,944,945,946,948,949,950,951,952,953,954,970,971,972,973,975,977,988,989"
-TYPE_PARAMS="${TYPE_PARAMS} --compoundFields subject=600\$a,610\$a,647\$a,648\$a,650\$a,651\$a,653\$a,654\$a"
+TYPE_PARAMS="${TYPE_PARAMS} --compoundFields subject=600\$a,610\$a,647\$a,648\$a,650\$a,651\$a,654\$a"
 # TYPE_PARAMS="${TYPE_PARAMS} --solrForScoresUrl http://localhost:8983/solr/kbr_scores"
 # index parameters
 TYPE_PARAMS="${TYPE_PARAMS} --indexWithTokenizedField --indexFieldCounts --indexSubfieldCounts"
